957cb2e582dd1c7f4247b18f14b1fcd1891f2524
jasminaj <jasmina_jakonic@yahoo.com>
2021-06-21 10:18:11 +0200

    inter font & mml font icons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react"
 
 const App = () => {
-  return <div className="App">Hello MLM :)</div>
+  return (
+    <div className="App">
+      Hello MLM :)
+      <span
+        className="Icon-Experimentations"
+        style={{ color: "orange", fontSize: "30px" }}
+      ></span>
+    </div>
+  )
 }
 
 export default App
