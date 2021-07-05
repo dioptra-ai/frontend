ef74644fe0d5dc1f8ccba46a4659ad8334600ddb
nenad12 <nenad@ggstud.io>
2021-07-05 14:12:48 +0200

    fixed lint script to include backend code as well

--- src/server/controllers/user.controller.mjs
+++ src/server/controllers/user.controller.mjs
@@ -2,10 +2,12 @@ import express from 'express';
 const UserRouter = express.Router();
 
 // define the home page route
+// eslint-disable-next-line no-unused-vars
 UserRouter.get('/', function (req, res, next) {
     res.send('USERS home page');
 });
 // define the about route
+// eslint-disable-next-line no-unused-vars
 UserRouter.get('/about', function (req, res, next) {
     res.send('About users');
 });
