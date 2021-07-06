74091de57ed3da458d5570453dd41b7df4eb202f
milosban <milosb@ggstud.io>
2021-07-06 14:56:00 +0200

    Implemente express-session

--- index.mjs
+++ index.mjs
@@ -5,18 +5,47 @@ import express from 'express';
 import {dirname, join, resolve} from 'path';
 import {fileURLToPath} from 'url';
 
+import session from 'express-session';
+import MongoStore from 'connect-mongo';
+
 const app = express();
 const basePath = dirname(fileURLToPath(import.meta.url));
 
 app.use(express.static(join(basePath, '/src/client/build')));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+const sessionStore = new MongoStore({
+    mongoUrl: process.env.DB_CONNECTION_URI,
+    collectionName: 'sessions'
+});
+
+const sessionHandler = session({
+    secret: 'some nice secret',
+    resave: false,
+    saveUninitialized: true,
+    store: sessionStore,
+    cookie: {
+        maxAge: Number(process.env.COOKIE_MAX_AGE)
+    }
+});
+
+// Middleware to use Session only on api routes
+app.use(function(req, res, next) {
+    if (req.url.indexOf('/api/') === 0) {
+        return sessionHandler(req, res, next);
+    } else {
+        return next();
+    }
+});
 
 // Register all controller routes to /api/ basepath
 import ApiRouter from './src/server/api-router.mjs';
 
 app.use('/api', ApiRouter);
 
-// Serve frontend
-app.get('*', (req, res, next) => {
+// Serve frontend on all routes other than /api
+app.get('*', (req, res) => {
     res.sendFile(resolve(basePath, 'src', 'client', 'build', 'index.html'));
 });
 
