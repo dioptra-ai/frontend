b21b133d53129f4450a23d482b0af4a1bad84349
Jacques Arnoux <jacques@dioptra.ai>
2021-07-14 16:32:21 -0700

    populating models page with contents of mobx store

--- src/client/src/pages/templates/models.js
+++ src/client/src/pages/templates/models.js
@@ -1,15 +1,47 @@
 import {Link} from 'react-router-dom';
-import GeneralSearchBar from './general-search-bar';
 import React from 'react';
+import PropTypes from 'prop-types';
+import {Col, Container, Row} from 'react-bootstrap';
+
+import GeneralSearchBar from './general-search-bar';
 import {setupComponent} from '../../helpers/component-helper';
 
-const Models = () => {
+const Models = ({modelStore}) => {
     return (
         <>
             <GeneralSearchBar shouldShowOnlySearchInput={true} />
-            <Link to='/models/1/performance-overview'>Go to Model</Link>
+            <Container>
+                <Row>
+                    <Col>
+                        Model Name
+                    </Col>
+                    <Col xs={2}>Owner</Col>
+                    <Col xs={2}>Tier</Col>
+                    <Col xs={2}>Last Deployed</Col>
+                </Row>
+                {modelStore.models.map((model) => (
+                    <Row key={model.mlModelId}>
+                        <Col>
+                            <Link to={`/models/${model.mlModelId}/performance-overview`}><b>{model.name}</b></Link>
+                        </Col>
+                        <Col xs={2}>
+                            {model.team?.name}
+                        </Col>
+                        <Col xs={2}>
+                            {model.tier}
+                        </Col>
+                        <Col xs={2}>
+                            {model.lastDeployed}
+                        </Col>
+                    </Row>
+                ))}
+            </Container>
         </>
     );
+};
+
+Models.propTypes = {
+    modelStore: PropTypes.object
 };
 
 export default setupComponent(Models);
