2ea86420171c4b0006d6112a8120e9f0f681732a
Jacques Arnoux <jacques@dioptra.ai>
2021-07-21 20:37:41 -0700

    Finish models list page (#57)
    * fixing the models page
    
    * pretty models page

--- src/client/src/helpers/date-helper.js
+++ src/client/src/helpers/date-helper.js
@@ -7,8 +7,8 @@ const last = (amount, periodName) => {
     return [start, end];
 };
 
-export const formatDate = (m) => m.toDate().toLocaleDateString();
-export const formatTime = (m) => m.toDate().toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'});
+export const formatDate = (m) => moment(m).toDate().toLocaleDateString();
+export const formatTime = (m) => moment(m).toDate().toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'});
 export const formatDateTime = (m) => `${formatDate(m)} ${formatTime(m)}`;
 export const formatDateRange = (momentDateFrom, momentDateTo) => `${formatDate(momentDateFrom)} - ${formatDate(momentDateTo)}`;
 export const formatDateTimeRange = (momentDateFrom, momentDateTo) => `${formatDateTime(momentDateFrom)} - ${formatDateTime(momentDateTo)}`;
--- src/client/src/pages/templates/models.js
+++ src/client/src/pages/templates/models.js
@@ -1,50 +1,127 @@
-import {Link} from 'react-router-dom';
-import React, {useEffect} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
+import Tooltip from 'react-bootstrap/Tooltip';
+import Overlay from 'react-bootstrap/Overlay';
+import Table from 'react-bootstrap/Table';
 import PropTypes from 'prop-types';
-import {Col, Container, Row} from 'react-bootstrap';
+import {Button} from 'react-bootstrap';
+import {Link} from 'react-router-dom';
 
 import GeneralSearchBar from './general-search-bar';
-import {setupComponent} from '../../helpers/component-helper';
-import {ModelStore} from '../../state/stores/model-store';
+import {setupComponent} from 'helpers/component-helper';
+import {formatDateTime} from 'helpers/date-helper';
+import Pagination from 'components/pagination';
+import FontIcon from 'components/font-icon';
+import {IconNames} from 'constants';
+import {Area, ComposedChart, Line} from 'recharts';
+import theme from 'styles/theme.module.scss';
+
+const NUMBER_OF_RECORDS_PER_PAGE = 10;
+
+const IncidentsTooltipContent = ({incidents}) => {
+    return (
+        <div className='bg-warning text-white p-3'>
+            {incidents.map((i, idx) => <div className='my-2 text-start d-flex' key={idx}>
+                <div className='me-2'><FontIcon className={'text-white'} icon={IconNames.WARNING} size={20}/></div>
+                <div>{i}</div>
+            </div>)}
+        </div>
+    );
+};
+
+IncidentsTooltipContent.propTypes = {
+    incidents: PropTypes.array
+};
+
+const ModelRow = ({model, idx, color}) => {
+    const incidentsRef = useRef(null);
+    const hasIncidents = false;
+    const [shouldShowTooltip, setShouldShowTooltip] = useState(false);
+
+    return (
+        <tr className='py-5' key={idx}>
+            <td className='py-2 align-middle'><Link className='cursor-pointer' to={`/models/${model._id}/performance-overview`}>{model.name}</Link></td>
+            <td className='py-2 align-middle'>
+                <div className='d-flex align-items-center justify-content-center cursor-pointer'
+                    onMouseEnter={() => setShouldShowTooltip(hasIncidents && true)}
+                    onMouseLeave={() => setShouldShowTooltip(hasIncidents && false)}
+                    ref={incidentsRef}>
+                    <FontIcon className={`text-${hasIncidents ? 'warning' : 'success'}`} icon={hasIncidents ? IconNames.WARNING : IconNames.CHECK} size={25} />
+                    {hasIncidents ? <span className='text-decoration-underline text-warning align-middle ms-1'>{model.incidents.length}</span> : null}
+                </div>
+                {hasIncidents ?
+                    <Overlay bsPrefix={'warning'} className='bg-warning' show={shouldShowTooltip} target={incidentsRef.current}>
+                        {(props) => <Tooltip id={'modelsTooltip'} {...props}><IncidentsTooltipContent incidents={model.incidents}/></Tooltip>}
+                    </Overlay> :
+                    null }
+            </td>
+            <td className='py-2 align-middle'>{model.project}</td>
+            <td className='py-2 align-middle'>{model.owner}</td>
+            <td className='py-2 align-middle'>{model.mlModelTier}</td>
+            <td className='py-2 align-middle'>{formatDateTime(model.lastDeployed)}</td>
+            <td className='py-2 align-middle'>
+                <div className='d-flex align-items-center justify-content-center'>
+                    <ComposedChart data={model.traffic} height={65} width={140}>
+                        <defs>
+                            <linearGradient id='areaColor' x1='0' x2='0' y1='0' y2='1'>
+                                <stop offset='5%' stopColor={color} stopOpacity={0.7}/>
+                                <stop offset='95%' stopColor='#FFFFFF' stopOpacity={0.1}/>
+                            </linearGradient>
+                        </defs>
+                        <Line connectNulls dataKey='y' dot={false} fill={color} stroke={color} strokeWidth={1} type='linear'/>
+                        <Area dataKey='y' fill='url(#areaColor)' stroke={color} strokeWidth={1} type='linear' />
+                    </ComposedChart>
+                </div>
+            </td>
+        </tr>
+    );
+};
+
+ModelRow.propTypes = {
+    color: PropTypes.string,
+    idx: PropTypes.number,
+    model: PropTypes.object
+};
 
 const Models = ({modelStore}) => {
-    useEffect(() => {
+    const [pageNumber, setPageNumber] = useState(0);
 
+    const color = theme.primary;
+    const totalPages = Math.ceil(modelStore.models.length / NUMBER_OF_RECORDS_PER_PAGE);
+    const data = modelStore.models.slice(pageNumber * NUMBER_OF_RECORDS_PER_PAGE, (pageNumber + 1) * NUMBER_OF_RECORDS_PER_PAGE);
+
+    useEffect(() => {
         modelStore.fetchModels();
     }, []);
 
     return (
         <>
             <GeneralSearchBar shouldShowOnlySearchInput={true} />
-            <Container>
-                <Row>
-                    <Col>
-                        <b>Model Name</b>
-                    </Col>
-                    <Col xs={2}><b>Owner</b></Col>
-                    <Col xs={2}><b>Tier</b></Col>
-                    <Col xs={2}><b>Last Deployed</b></Col>
-                </Row>
-                {
-                    modelStore.state === ModelStore.STATE_PENDING ? 'Loading...' :
-                        modelStore.models.map((model) => (
-                            <Row key={model._id}>
-                                <Col>
-                                    <Link to={`/models/${model._id}/performance-overview`}><b>{model.name}</b></Link>
-                                </Col>
-                                <Col xs={2}>
-                                    {model.team?.name}
-                                </Col>
-                                <Col xs={2}>
-                                    {model.tier}
-                                </Col>
-                                <Col xs={2}>
-                                    {model.lastDeployed}
-                                </Col>
-                            </Row>
-                        ))
-                }
-            </Container>
+            {
+
+                modelStore.state === modelStore.STATE_PENDING ? 'Loading...' : (
+                    <div className='p-4 mt-5'>
+                        <div className='d-flex justify-content-between'>
+                            <span className='h2 fs-2 fw-bold'>Models</span>
+                            <span><Button className='py-3 px-5 text-white' variant='primary'>REGISTER MODEL</Button></span>
+                        </div>
+                        <div className='my-5'>
+                            <Table className='models-table'>
+                                <thead className='text-secondary border-bottom border-light'>
+                                    <tr>
+                                        {['Model Name', 'Open Incidents', 'Project', 'Owner', 'Tier', 'Last Deployed', 'Traffic'].map((c, idx) => (
+                                            <th className={`text-secondary py-4 ${idx === 0 ? 'w-25' : ''} ${idx === 1 || idx === 6 ? 'text-center' : ''}`} key={c}>{c}</th>))
+                                        }
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {data.map((model) => <ModelRow color={color} key={model._id} model={model}/>)}
+                                </tbody>
+                            </Table>
+                        </div>
+                        <Pagination onPageChange={setPageNumber} totalPages={totalPages} />
+                    </div>
+                )
+            }
         </>
     );
 };
