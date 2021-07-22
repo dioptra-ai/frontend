add54e1c86dc90278e28e811bebe4147f1153e6e
Jacques Arnoux <jacques@dioptra.ai>
2021-07-21 20:35:40 -0700

    pretty models page

--- src/client/src/pages/templates/models.js
+++ src/client/src/pages/templates/models.js
@@ -1,20 +1,19 @@
 import React, {useEffect, useRef, useState} from 'react';
-import {action} from 'mobx';
 import Tooltip from 'react-bootstrap/Tooltip';
 import Overlay from 'react-bootstrap/Overlay';
 import Table from 'react-bootstrap/Table';
 import PropTypes from 'prop-types';
 import {Button} from 'react-bootstrap';
-import {useHistory} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
 import GeneralSearchBar from './general-search-bar';
 import {setupComponent} from 'helpers/component-helper';
+import {formatDateTime} from 'helpers/date-helper';
 import Pagination from 'components/pagination';
 import FontIcon from 'components/font-icon';
 import {IconNames} from 'constants';
 import {Area, ComposedChart, Line} from 'recharts';
 import theme from 'styles/theme.module.scss';
-import {Paths} from 'configs/route-config';
 
 const NUMBER_OF_RECORDS_PER_PAGE = 10;
 
@@ -33,36 +32,35 @@ IncidentsTooltipContent.propTypes = {
     incidents: PropTypes.array
 };
 
-const ModelRow = ({m, idx, handleCellClickRedirect, color}) => {
+const ModelRow = ({model, idx, color}) => {
     const incidentsRef = useRef(null);
     const hasIncidents = false;
     const [shouldShowTooltip, setShouldShowTooltip] = useState(false);
 
     return (
         <tr className='py-5' key={idx}>
-            <td className='py-2 align-middle'><span className='cursor-pointer' onClick={() => handleCellClickRedirect(m.id, 'MODEL_PERFORMANCE_OVERVIEW')}>{m.name}</span></td>
+            <td className='py-2 align-middle'><Link className='cursor-pointer' to={`/models/${model._id}/performance-overview`}>{model.name}</Link></td>
             <td className='py-2 align-middle'>
                 <div className='d-flex align-items-center justify-content-center cursor-pointer'
-                    onClick={() => handleCellClickRedirect(m.id, 'MODEL_INCIDENTS_AND_ALERTS')}
                     onMouseEnter={() => setShouldShowTooltip(hasIncidents && true)}
                     onMouseLeave={() => setShouldShowTooltip(hasIncidents && false)}
                     ref={incidentsRef}>
                     <FontIcon className={`text-${hasIncidents ? 'warning' : 'success'}`} icon={hasIncidents ? IconNames.WARNING : IconNames.CHECK} size={25} />
-                    {hasIncidents ? <span className='text-decoration-underline text-warning align-middle ms-1'>{m.incidents.length}</span> : null}
+                    {hasIncidents ? <span className='text-decoration-underline text-warning align-middle ms-1'>{model.incidents.length}</span> : null}
                 </div>
                 {hasIncidents ?
                     <Overlay bsPrefix={'warning'} className='bg-warning' show={shouldShowTooltip} target={incidentsRef.current}>
-                        {(props) => <Tooltip id={'modelsTooltip'} {...props}><IncidentsTooltipContent incidents={m.incidents}/></Tooltip>}
+                        {(props) => <Tooltip id={'modelsTooltip'} {...props}><IncidentsTooltipContent incidents={model.incidents}/></Tooltip>}
                     </Overlay> :
                     null }
             </td>
-            <td className='py-2 align-middle'>{m.project}</td>
-            <td className='py-2 align-middle'>{m.owner}</td>
-            <td className='py-2 align-middle'>{m.tier}</td>
-            <td className='py-2 align-middle'>{m.deployed}</td>
+            <td className='py-2 align-middle'>{model.project}</td>
+            <td className='py-2 align-middle'>{model.owner}</td>
+            <td className='py-2 align-middle'>{model.mlModelTier}</td>
+            <td className='py-2 align-middle'>{formatDateTime(model.lastDeployed)}</td>
             <td className='py-2 align-middle'>
                 <div className='d-flex align-items-center justify-content-center'>
-                    <ComposedChart data={m.traffic} height={65} width={140}>
+                    <ComposedChart data={model.traffic} height={65} width={140}>
                         <defs>
                             <linearGradient id='areaColor' x1='0' x2='0' y1='0' y2='1'>
                                 <stop offset='5%' stopColor={color} stopOpacity={0.7}/>
@@ -80,23 +78,16 @@ const ModelRow = ({m, idx, handleCellClickRedirect, color}) => {
 
 ModelRow.propTypes = {
     color: PropTypes.string,
-    handleCellClickRedirect: PropTypes.func,
     idx: PropTypes.number,
-    m: PropTypes.object
+    model: PropTypes.object
 };
 
 const Models = ({modelStore}) => {
-    const history = useHistory();
     const [pageNumber, setPageNumber] = useState(0);
 
     const color = theme.primary;
     const totalPages = Math.ceil(modelStore.models.length / NUMBER_OF_RECORDS_PER_PAGE);
     const data = modelStore.models.slice(pageNumber * NUMBER_OF_RECORDS_PER_PAGE, (pageNumber + 1) * NUMBER_OF_RECORDS_PER_PAGE);
-
-    const handleCellClickRedirect = action((modelId, pathName) => {
-        modelStore.activeModelId = modelId;
-        history.push(Paths(modelId)[pathName]);
-    });
 
     useEffect(() => {
         modelStore.fetchModels();
@@ -123,7 +114,7 @@ const Models = ({modelStore}) => {
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    {data.map((m, idx) => <ModelRow color={color} handleCellClickRedirect={handleCellClickRedirect} idx={idx} key={idx} m={m}/>)}
+                                    {data.map((model) => <ModelRow color={color} key={model._id} model={model}/>)}
                                 </tbody>
                             </Table>
                         </div>
