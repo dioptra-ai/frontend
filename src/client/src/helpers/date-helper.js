0a0596cd93c2f7c4ed18bef015c675c107bbe3d0
Jacques Arnoux <jacques@dioptra.ai>
2021-07-22 21:50:02 -0700

    enabled the refresh button to refresh the time range

--- src/client/src/helpers/date-helper.js
+++ src/client/src/helpers/date-helper.js
@@ -13,6 +13,7 @@ export const formatDateTime = (m) => `${formatDate(m)} ${formatTime(m)}`;
 export const formatDateRange = (momentDateFrom, momentDateTo) => `${formatDate(momentDateFrom)} - ${formatDate(momentDateTo)}`;
 export const formatDateTimeRange = (momentDateFrom, momentDateTo) => `${formatDateTime(momentDateFrom)} - ${formatDateTime(momentDateTo)}`;
 
+export const lastSeconds = (seconds) => last(seconds, 'seconds');
 export const lastDays = (days) => last(days, 'days');
 export const lastHours = (hours) => last(hours, 'hours');
 export const lastMinutes = (minutes) => last(minutes, 'minutes');
--- src/client/src/state/stores/time-store.js
+++ src/client/src/state/stores/time-store.js
@@ -1,6 +1,7 @@
+import moment from 'moment';
 import {makeAutoObservable} from 'mobx';
 
-import {lastHours} from 'helpers/date-helper';
+import {lastHours, lastSeconds} from 'helpers/date-helper';
 
 // TODO: restore this from local storage.
 const [initialStart, initialEnd] = lastHours(24);
@@ -10,13 +11,31 @@ class TimeStore {
 
     end = initialEnd;
 
+    // This is true for all "Last xxx minute/hours/days" type time ranges, and false for custom, fixed ranges.
+    refreshable = true;
+
     constructor() {
         makeAutoObservable(this);
     }
 
     setTimeRange({start, end}) {
+        const now = moment();
+
         this.start = start;
         this.end = end;
+
+        // If the end is very close to now, assume we mean now and make it refreshable.
+        // Date picker granulatiry is 1 minute == 60000 ms.
+        this.refreshable = now.diff(end) <= 60000;
+    }
+
+    refreshTimeRange() {
+        if (this.refreshable) {
+            const rangeMillisec = this.end.diff(this.start);
+            const [start, end] = lastSeconds(rangeMillisec / 1000);
+
+            this.setTimeRange({start, end});
+        }
     }
 
     get sQLTimeFilter() {
