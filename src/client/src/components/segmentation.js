43a8dbfbbe1fff85fbac4f8f759c5c9f7d668320
Jacques Arnoux <jacques@dioptra.ai>
2021-07-30 13:23:29 -0700

    fixed default data

--- src/client/src/components/segmentation.js
+++ src/client/src/components/segmentation.js
@@ -259,6 +259,7 @@ const Segmentation = ({timeStore}) => {
                     </Button>
                 </div>
                 <TimeseriesQuery
+                    defaultData={[]}
                     renderData={(data) => (
                         <Table
                             columns={
