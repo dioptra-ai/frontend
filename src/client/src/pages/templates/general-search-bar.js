f08e932e226f1970c4e9667f702ff9ef4d9e6778
Ahmic, Adnan (FR - External) <adnan@ggstud.io>
2021-08-11 10:30:12 +0200

    WIP: further enchancements

--- src/client/src/pages/templates/general-search-bar.js
+++ src/client/src/pages/templates/general-search-bar.js
@@ -12,93 +12,92 @@ import TextInput from 'components/text-input';
 import {setupComponent} from 'helpers/component-helper';
 
 const GeneralSearchBar = ({
-  shouldShowOnlySearchInput,
-  timeStore,
-  location,
-  history
+    shouldShowOnlySearchInput,
+    timeStore,
+    location,
+    history
 }) => {
-  const {startMoment, endMoment} = timeStore;
+    const {startMoment, endMoment} = timeStore;
 
-  const [moments, setMoments] = useState({
-    startStateMoment: startMoment,
-    endStateMoment: endMoment
-  });
-
-  const {startTime, endTime, ...restQueryString} = useMemo(
-    () => qs.parse(location?.search, {ignoreQueryPrefix: true}),
-    [location?.search]
-  );
-
-  const startTimeMoment = useMemo(
-    () => (startTime && moment(startTime).isValid() ? moment(startTime) : false),
-    [startTime]
-  );
-  const endTimeMoment = useMemo(
-    () => (endTime && moment(endTime).isValid() ? moment(endTime) : false),
-    [endTime]
-  );
-
-  const handleDateChange = ({start, end}) => {
-    timeStore.setTimeRange({start, end});
-    setMoments({startStateMoment: start, endStateMoment: end});
-  };
-
-  useEffect(() => {
-    if (startTimeMoment && endTimeMoment)
-      handleDateChange({start: startTimeMoment, end: endTimeMoment});
-  }, []);
-
-  useEffect(() => {
-    const {startStateMoment, endStateMoment} = moments;
-
-    history.replace({
-      pathname: location.pathname,
-      search: qs.stringify({
-        ...restQueryString,
-        startTime: startStateMoment.toISOString(),
-        endTime: endStateMoment.toISOString()
-      })
+    const [moments, setMoments] = useState({
+        startStateMoment: startMoment,
+        endStateMoment: endMoment
     });
-  }, [moments]);
 
-  return (
-    <div className="py-3 px-4 d-flex align-items-center border-bottom">
-      <FontIcon className="text-secondary" icon={IconNames.SEARCH} size={25} />
-      <div className="flex-grow-1 mx-3">
-        <TextInput
-          className="form-control border-0 py-3 fs-5"
-          placeholder="Search"
-        />
-      </div>
-      {shouldShowOnlySearchInput ? null : (
-        <>
-          <DateTimeRangePicker
-            end={moments.endStateMoment}
-            onChange={handleDateChange}
-            start={moments.startStateMoment}
-          />
-          <Button
-            className="text-white d-flex align-items-center justify-content-between px-4 py-2 ms-3"
-            disabled={!timeStore.refreshable}
-            onClick={() => timeStore.refreshTimeRange()}
-            variant="primary"
-          >
-            <FontIcon
-              className="text-white m-2"
-              icon={IconNames.REFRESH}
-              size={15}
-            />
-            <span className="fs-6 bold-text">REFRESH</span>
-          </Button>
-        </>
-      )}
-    </div>
-  );
+    const {startTime, endTime, ...restQueryString} = useMemo(
+        () => qs.parse(location?.search, {ignoreQueryPrefix: true}),
+        [location?.search]
+    );
+
+    const startTimeMoment = useMemo(
+        () => (startTime && moment(startTime).isValid() ? moment(startTime) : false),
+        [startTime]
+    );
+    const endTimeMoment = useMemo(
+        () => (endTime && moment(endTime).isValid() ? moment(endTime) : false),
+        [endTime]
+    );
+
+    const handleDateChange = ({start, end}) => {
+        timeStore.setTimeRange({start, end});
+        setMoments({startStateMoment: start, endStateMoment: end});
+    };
+
+    useEffect(() => {
+        if (startTimeMoment && endTimeMoment) handleDateChange({start: startTimeMoment, end: endTimeMoment});
+    }, []);
+
+    useEffect(() => {
+        const {startStateMoment, endStateMoment} = moments;
+
+        history.replace({
+            pathname: location.pathname,
+            search: qs.stringify({
+                ...restQueryString,
+                startTime: startStateMoment.toISOString(),
+                endTime: endStateMoment.toISOString()
+            })
+        });
+    }, [moments]);
+
+    return (
+        <div className='py-3 px-4 d-flex align-items-center border-bottom'>
+            <FontIcon className='text-secondary' icon={IconNames.SEARCH} size={25} />
+            <div className='flex-grow-1 mx-3'>
+                <TextInput
+                    className='form-control border-0 py-3 fs-5'
+                    placeholder='Search'
+                />
+            </div>
+            {shouldShowOnlySearchInput ? null : (
+                <>
+                    <DateTimeRangePicker
+                        end={moments.endStateMoment}
+                        onChange={handleDateChange}
+                        start={moments.startStateMoment}
+                    />
+                    <Button
+                        className='text-white d-flex align-items-center justify-content-between px-4 py-2 ms-3'
+                        disabled={!timeStore.refreshable}
+                        onClick={() => timeStore.refreshTimeRange()}
+                        variant='primary'
+                    >
+                        <FontIcon
+                            className='text-white m-2'
+                            icon={IconNames.REFRESH}
+                            size={15}
+                        />
+                        <span className='fs-6 bold-text'>REFRESH</span>
+                    </Button>
+                </>
+            )}
+        </div>
+    );
 };
 
 GeneralSearchBar.propTypes = {
-  shouldShowOnlySearchInput: PropTypes.bool,
-  timeStore: PropTypes.object
+    shouldShowOnlySearchInput: PropTypes.bool,
+    timeStore: PropTypes.object
 };
 
 export default setupComponent(withRouter(GeneralSearchBar));
