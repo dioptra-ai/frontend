6a0bf532d90e8d764b21da46fe825ccd065c286d
Jacques Arnoux <jacques@dioptra.ai>
2021-08-24 13:23:45 -0700

    added babel polyfill to client-side code to allows for async/await

--- webpack.config.js
+++ webpack.config.js
@@ -36,7 +36,8 @@ module.exports = {
                     options: {
                         presets: ['@babel/preset-env', ['@babel/preset-react', {
                             runtime: 'automatic'
-                        }]]
+                        }]],
+                        plugins: ['@babel/plugin-transform-runtime']
                     }
                 }
             },
