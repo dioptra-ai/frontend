8f442f15510c74214546e998e4fcec3246e24661
Jacques Arnoux <jacques@dioptra.ai>
2021-09-08 11:22:51 -0700

    organization models
    organization models

--- src/server/models/organization-membership.mjs
+++ src/server/models/organization-membership.mjs
@@ -1,9 +1,8 @@
 import mongoose from 'mongoose';
 
-const Schema = mongoose.Schema;
-const organizationMembershipSchema = new Schema({
-    user: {type: Schema.Types.ObjectId, ref: 'User', required: true},
-    organization: {type: Schema.Types.ObjectId, ref: 'Organization', required: true},
+const organizationMembershipSchema = new mongoose.Schema({
+    userId: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
+    teamId: {type: mongoose.Schema.Types.ObjectId, ref: 'Organization', required: true},
     type: { // i think this needs typePojoToMixed: false
         type: String,
         enum: ['ADMIN', 'MEMBER'],
