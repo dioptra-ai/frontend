0d0a9f16fa227c54f9ee42ecdceddfdd74b6e70d
pharnoux <pierrehadrien.arnoux@gmail.com>
2021-10-06 10:42:06 -0400

    First draft org id

--- src/client/src/customHooks/use-all-sql-filters.js
+++ src/client/src/customHooks/use-all-sql-filters.js
@@ -2,20 +2,24 @@ import {useParams} from 'react-router-dom';
 
 import stores from 'state/stores';
 
-const {filtersStore, timeStore, modelStore} = stores;
+const {filtersStore, timeStore, modelStore, authStore} = stores;
 
 const useAllSqlFilters = ({useReferenceRange = false} = {}) => {
     const params = useParams();
     const activeModelId = params._id;
+    const organizationId = authStore.userData.activeOrganizationMembership?.organization._id;
+
+    console.log(organizationId);
 
     if (activeModelId) {
         const activeModel = modelStore.getModelById(activeModelId);
         const timeFilter = useReferenceRange ? modelStore.getSqlReferencePeriodFilter(activeModelId) : timeStore.sqlTimeFilter;
 
-        return `${timeFilter} AND ${filtersStore.sqlFilters} AND ${activeModelId ? `model_id='${activeModel.mlModelId}'` : 'TRUE'}`;
+        return `${timeFilter} AND ${filtersStore.sqlFilters} AND organization_id=${organizationId}` +
+               ` AND ${activeModelId ? `model_id='${activeModel.mlModelId}'` : 'TRUE'}`;
     } else {
 
-        return `${timeStore.sqlTimeFilter} AND ${filtersStore.sqlFilters}`;
+        return `${timeStore.sqlTimeFilter} AND ${filtersStore.sqlFilters} AND organization_id=${organizationId}`;
     }
 
 };
