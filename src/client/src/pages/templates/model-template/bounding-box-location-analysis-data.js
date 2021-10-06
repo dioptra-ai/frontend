740992c736fc012478f462dcd5db385bcc3f8477
Ahmic, Adnan (FR - External) <adnan@ggstud.io>
2021-10-06 13:16:32 +0200

    Bouding box logic

--- src/client/src/pages/templates/model-template/bounding-box-location-analysis-data.js
+++ src/client/src/pages/templates/model-template/bounding-box-location-analysis-data.js
@@ -6,7 +6,25 @@ export default [
         // Drives the preview panel on the right
         samples: [
             {
-                image_url: 'https://xyz.com',
+                image_url: 'https://fakeimg.pl/',
+                width: 200,
+                height: 300,
+                // a red rectangle (box) has to be drawn on top of the given image.
+                // x is the horizontal coordinate (from left to right) of the top left corner of the box.
+                // y is the vertical coordinate (from top to bottom) of the top left corner of the box.
+
+                // WARNING: x, y, height and width are in number of pixels of the parent image.
+                // This means the height and width of the red box on screen will depend on the
+                // size of the <img/> element. Similarly for x and y.
+                bounding_box: {
+                    height: 20,
+                    width: 30,
+                    x: 20,
+                    y: 100 // ints
+                }
+            },
+            {
+                image_url: 'https://fakeimg.pl/',
                 width: 200,
                 height: 300,
                 // a red rectangle (box) has to be drawn on top of the given image.
@@ -24,7 +42,7 @@ export default [
                 }
             },
             {
-                image_url: 'https://xyz.com',
+                image_url: 'https://fakeimg.pl/',
                 width: 200,
                 height: 300,
                 // a red rectangle (box) has to be drawn on top of the given image.
@@ -35,32 +53,14 @@ export default [
                 // This means the height and width of the red box on screen will depend on the
                 // size of the <img/> element. Similarly for x and y.
                 bounding_box: {
-                    height: 20,
-                    width: 30,
-                    x: 2,
+                    height: 80,
+                    width: 100,
+                    x: 120,
                     y: 10 // ints
                 }
             },
             {
-                image_url: 'https://xyz.com',
-                width: 200,
-                height: 300,
-                // a red rectangle (box) has to be drawn on top of the given image.
-                // x is the horizontal coordinate (from left to right) of the top left corner of the box.
-                // y is the vertical coordinate (from top to bottom) of the top left corner of the box.
-
-                // WARNING: x, y, height and width are in number of pixels of the parent image.
-                // This means the height and width of the red box on screen will depend on the
-                // size of the <img/> element. Similarly for x and y.
-                bounding_box: {
-                    height: 20,
-                    width: 30,
-                    x: 2,
-                    y: 10 // ints
-                }
-            },
-            {
-                image_url: 'https://xyz.com',
+                image_url: 'https://fakeimg.pl/',
                 width: 200,
                 height: 300,
                 // a red rectangle (box) has to be drawn on top of the given image.
--- src/client/src/pages/templates/model-template/prediction-analysis.js
+++ src/client/src/pages/templates/model-template/prediction-analysis.js
@@ -21,14 +21,27 @@ const PredictionAnalysis = ({timeStore, filtersStore}) => {
     const allOfflineSqlFilters = useAllSqlFilters({useReferenceRange: true});
     const timeGranularity = timeStore.getTimeGranularity().toISOString();
     const [classFilter, setClassFilter] = useState('all_classes');
+    const [selectedPoint, setSelectedPoint] = useState(null);
 
     const {mlModelType} = useModel();
 
+    const samples = selectedPoint?.samples || [];
+
     const heatmapData = new Array(20)
         .fill([])
-        .map((_, i) => data.filter(({y}) => y === i).map(({x}) => x || 0));
+        .map((_, i) => new Array(20)
+            .fill(0)
+            .map((_, j) => data.find(({x, y}) => y === i && x === j)?.outlier || 0));
 
-    console.log(heatmapData);
+    // const dataNew = new Array(20)
+    //     .fill(0)
+    //     .map(() => new Array(20).fill(0).map(() => Math.floor(Math.random() * 100)));
+
+    const handleClick = (_x, _y) => {
+        const outlierData = data.find(({x, y}) => x === _y && y === _x);
+
+        setSelectedPoint(outlierData);
+    };
 
     return (
         <>
@@ -281,32 +294,90 @@ const PredictionAnalysis = ({timeStore, filtersStore}) => {
                                     onChange={setClassFilter}
                                 />
                             </Col>
-                            <Col className='d-flex' lg={4}>
+                            <Col lg={4}>
                                 <TimeseriesQuery
                                     defaultData={[]}
                                     renderData={() => (
-                                        <HeatMapGrid data={heatmapData} xLabels={null} yLabels={null} />
+                                        <div className='heat-map'>
+                                            <HeatMapGrid
+                                                // data={dataNew}
+                                                data={heatmapData}
+                                                cellStyle={() => ({
+                                                    maxWidth: 27,
+                                                    width: '100%',
+                                                    height: 30,
+                                                    borderRadius: 0,
+                                                    borderWidth: '1px 1px 1px 1px',
+                                                    borderStyle: 'dashed',
+                                                    borderColor: '#E5E5E5',
+                                                    background: 'none'
+                                                })}
+                                                cellRender={(_, __, value) => (
+                                                    <div
+                                                        className='heat-map-cell'
+                                                        style={{
+                                                            background:
+                                value <= 50 ?
+                                    `rgba(31, 169, 200, ${value / 50})` :
+                                    `rgba(248, 136, 108, ${value / 100})`
+                                                        }}
+                                                    />
+                                                )}
+                                                onClick={handleClick}
+                                            />
+                                            <div className='w-100 my-4 d-flex'>
+                                                <div className='d-flex align-items-center'>
+                                                    <div className='heat-map-legend heat-map-legend_red' />
+                                                    <span className='text-secondary heat-map-legend-text'>
+                            Outlier
+                                                    </span>
+                                                </div>
+                                                <div className='d-flex align-items-center'>
+                                                    <div className='heat-map-legend heat-map-legend_blue' />
+                                                    <span className='text-secondary heat-map-legend-text'>
+                            Non-Outlier
+                                                    </span>
+                                                </div>
+                                            </div>
+                                        </div>
                                     )}
                                     sql={sql`SELECT 1 as "one"`}
                                 />
                             </Col>
-                            <Col className='d-flex' lg={8}>
-                                <TimeseriesQuery
-                                    defaultData={[]}
-                                    renderData={(data) => (
-                                        <AreaGraph
-                                            dots={data}
-                                            title='Average'
-                                            unit='%'
-                                            xAxisDomain={timeStore.rangeMillisec}
-                                            xAxisName='Time'
-                                            yAxisName='Relative Coordinates (%)'
-                                            className='border-0 rounded-0'
-                                            hasBorder={false}
-                                        />
-                                    )}
-                                    sql={sql`SELECT 1 as "one"`}
-                                />
+                            <Col lg={8} className='rounded p-3 pt-0'>
+                                {samples.length ? (
+                                    <div
+                                        className={
+                                            'd-flex p-2 overflow-auto flex-grow-1 justify-content-left flex-wrap w-100 h-100 bg-white-blue'
+                                        }
+                                    >
+                                        {samples.map(({image_url, width, height, bounding_box}, i) => (
+                                            <div key={i} className='m-4 heat-map-item'>
+                                                <img
+                                                    alt='Example'
+                                                    className='rounded'
+                                                    src={`${image_url}${width}x${height}/`}
+                                                    height={height}
+                                                    width={width}
+                                                />
+                                                <div className='heat-map-box' style={{
+                                                    height: bounding_box?.height,
+                                                    width: bounding_box?.width,
+                                                    top: bounding_box?.y,
+                                                    left: bounding_box?.x
+                                                }}/>
+                                            </div>
+                                        ))}{' '}
+                                    </div>
+                                ) : (
+                                    <div
+                                        className={
+                                            'd-flex p-2 overflow-auto flex-grow-1 justify-content-center align-items-center w-100 h-100 bg-white-blue'
+                                        }
+                                    >
+                                        <h3 className='text-secondary m-0'>No Data Available</h3>
+                                    </div>
+                                )}
                             </Col>
                         </Row>
                     </div>
