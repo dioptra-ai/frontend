9a64d3f1441f7f386ac4e70787dc1c2a6c90c632
Jacques Arnoux <jacques@dioptra.ai>
2021-10-06 20:45:17 -0700

    added DELETE /api/api-key

--- src/server/controllers/api-key.mjs
+++ src/server/controllers/api-key.mjs
@@ -1,10 +1,10 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import {APIGatewayClient, CreateApiKeyCommand} from '@aws-sdk/client-api-gateway';
+// import {APIGatewayClient, CreateApiKeyCommand, DeleteApiKeyCommand} from '@aws-sdk/client-api-gateway';
 
 import {isAuthenticated} from '../middleware/authentication.mjs';
 
-const client = new APIGatewayClient({region: 'us-east-2'});
+// const client = new APIGatewayClient({region: 'us-east-2'});
 
 const ApiKeyRouter = express.Router();
 
@@ -13,25 +13,51 @@ ApiKeyRouter.all('*', isAuthenticated);
 ApiKeyRouter.post('/', async (req, res, next) => {
 
     try {
-        const Key = mongoose.model('ApiKey');
-        const dioptraUserId = req.user._id;
-        const dioptraOrganizationId = req.user.activeOrganizationMembership.organization._id;
-        const dioptraApiKey = new Key({
+        const ApiKey = mongoose.model('ApiKey');
+        // const dioptraUserId = req.user._id;
+        // const dioptraOrganizationId = req.user.activeOrganizationMembership.organization._id;
+        const dioptraApiKey = new ApiKey({
             user: req.user._id,
             organization: req.user.activeOrganizationMembership.organization._id
         });
-        const awsApiKey = await client.send(new CreateApiKeyCommand({
-            enabled: true,
-            tags: {
-                dioptraUserId, dioptraOrganizationId,
-                dioptraApiKeyId: dioptraApiKey._id
-            }
-        }));
+
+        // Hard-coding for now so Adnan can work.
+        const awsApiKey = {
+            id: 'foobar',
+            value: 'foobar'
+        };
+
+        // const awsApiKey = await client.send(new CreateApiKeyCommand({
+        //     enabled: true,
+        //     tags: {
+        //         dioptraUserId, dioptraOrganizationId,
+        //         dioptraApiKeyId: dioptraApiKey._id
+        //     }
+        // }));
 
         dioptraApiKey.awsApiKeyId = awsApiKey.id;
         dioptraApiKey.awsApiKey = awsApiKey.value;
 
-        res.json(dioptraApiKey.save());
+        res.json(await dioptraApiKey.save());
+    } catch (e) {
+        next(e);
+    }
+});
+
+ApiKeyRouter.delete('/:_id', async (req, res, next) => {
+
+    try {
+        const ApiKey = mongoose.model('ApiKey');
+        const dioptraApiKey = await ApiKey.findOneAndDelete({
+            _id: req.params._id,
+            user: req.user._id
+        });
+
+        // const awsApiKey = await client.send(new DeleteApiKeyCommand({
+        //     apiKey: dioptraApiKey.awsApiKeyId
+        // }));
+
+        res.json(dioptraApiKey);
     } catch (e) {
         next(e);
     }
