e8aa52715a4fd7f11382f5a4df675ba7e31bd3a4
Ahmic, Adnan (FR - External) <adnan@ggstud.io>
2021-10-11 18:09:29 +0200

    Multi-select using mouse

--- src/client/src/components/scatter-graph.js
+++ src/client/src/components/scatter-graph.js
@@ -2,6 +2,7 @@ import {useEffect, useRef, useState} from 'react';
 import {
     CartesianGrid,
     Legend,
+    ReferenceArea,
     ResponsiveContainer,
     Scatter,
     ScatterChart,
@@ -21,6 +22,12 @@ import Modal from 'components/modal';
 const LARGE_DOT_SIZE = 200;
 const MEDIUM_DOT_SIZE = 100;
 const SMALL_DOT_SIZE = 60;
+
+const min = (n, m) => (n < m ? n : m);
+const max = (n, m) => (n > m ? n : m);
+
+const inRange = (num, min, max) => num >= min && num <= max;
+
 const ScatterGraph = ({data}) => {
     const ref = useRef();
     const firstOutlier = data.find(({outlier}) => outlier);
@@ -31,6 +38,9 @@ const ScatterGraph = ({data}) => {
     const [exampleInModal, setExampleInModal] = useModal(false);
     const [shiftPressed, setShiftPressed] = useState(false);
     const [samples, setSamples] = useState([]);
+    const [refTopLeft, setRefTopLeft] = useState(null);
+    const [refBottomRight, setRefBottomRight] = useState(null);
+    const [multiSelect, setMultiSelect] = useState(false);
 
     const handleKeyDown = ({keyCode}) => {
         if (keyCode === 16) setShiftPressed(true);
@@ -51,6 +61,10 @@ const ScatterGraph = ({data}) => {
         };
     }, []);
 
+    useEffect(() => {
+        setSamples(selectedPoint?.samples || []);
+    }, [selectedPoint]);
+
     const handlePointSelect = (point) => {
         setSelectedPoint(point);
         if (shiftPressed) {
@@ -58,12 +72,45 @@ const ScatterGraph = ({data}) => {
         } else setSamples([...(point?.samples || [])]);
     };
 
+    const handleMouseUp = () => {
+        const x1 = min(refTopLeft?.x, refBottomRight?.x);
+        const x2 = max(refTopLeft?.x, refBottomRight?.x);
+        const y1 = min(refTopLeft?.y, refBottomRight?.y);
+        const y2 = max(refTopLeft?.y, refBottomRight?.y);
+
+        if (x1 && y1 && x2 && y2) {
+            const filteredData = data.filter(
+                ({PCA1, PCA2}) => inRange(PCA1, x1, x2) && inRange(PCA2, y1, y2)
+            );
+
+            const multiSamples = filteredData.reduce(
+                (samples, d) => [...samples, ...(d.samples || [])],
+                []
+            );
+
+            setSamples(multiSamples);
+        }
+        setMultiSelect(false);
+    };
+
     return (
         <>
             <Row className='border rounded p-3 w-100 scatterGraph' ref={ref}>
-                <Col lg={4} className='scatterGraph-leftBox'>
+                <Col lg={4} className='scatterGraph-leftBox' style={{userSelect: 'none'}}>
                     <ResponsiveContainer width='100%' height='100%'>
-                        <ScatterChart>
+                        <ScatterChart
+                            onMouseDown={(e) => {
+                                if (e?.xValue && e?.yValue) {
+                                    setRefTopLeft({x: e?.xValue, y: e?.yValue});
+                                    setMultiSelect(true);
+                                    setRefBottomRight(null);
+                                }
+                            }}
+                            onMouseUp={handleMouseUp}
+                            onMouseMove={(e) => {
+                                if (multiSelect) setRefBottomRight({x: e?.xValue, y: e?.yValue});
+                            }}
+                        >
                             <CartesianGrid strokeDasharray='6 2' stroke={theme.light} />
                             <XAxis
                                 type='number'
@@ -79,6 +126,7 @@ const ScatterGraph = ({data}) => {
                                 tickLine={false}
                                 tick={() => null}
                                 tickCount={10}
+                                xAxisId='PCA1'
                             />
                             <YAxis
                                 type='number'
@@ -95,6 +143,7 @@ const ScatterGraph = ({data}) => {
                                 tickLine={false}
                                 tick={() => null}
                                 tickCount={10}
+                                yAxisId='PCA2'
                             />
                             <ZAxis type='number' dataKey='size' range={[SMALL_DOT_SIZE, LARGE_DOT_SIZE]} />
                             <Legend wrapperStyle={{bottom: '-10px'}} fill='black'/>
@@ -114,40 +163,59 @@ const ScatterGraph = ({data}) => {
                                     ...d
                                 }))}
                                 fill={theme.warning}
+                                xAxisId='PCA1'
+                                yAxisId='PCA2'
                             />
                             <Scatter
                                 isAnimationActive={false}
                                 cursor='pointer'
-                                onClick={setSelectedPoint}
+                                onClick={handlePointSelect}
                                 name='Novelty'
                                 data={data.filter(({outlier, novelty}) => !outlier && novelty).map((d) => ({
                                     size: d.PCA1 === selectedPoint.PCA1 && d.PCA2 === selectedPoint.PCA2 ? LARGE_DOT_SIZE : MEDIUM_DOT_SIZE,
                                     ...d
                                 }))}
                                 fill={theme.success}
+                                xAxisId='PCA1'
+                                yAxisId='PCA2'
                             />
                             <Scatter
                                 isAnimationActive={false}
                                 cursor='pointer'
-                                onClick={setSelectedPoint}
+                                onClick={handlePointSelect}
                                 legendType='none'
                                 data={data.filter(({outlier, novelty}) => outlier && novelty).map((d) => ({
                                     size: d.PCA1 === selectedPoint.PCA1 && d.PCA2 === selectedPoint.PCA2 ? LARGE_DOT_SIZE : MEDIUM_DOT_SIZE,
                                     ...d
                                 }))}
                                 fill='url(#colorGrad)'
+                                xAxisId='PCA1'
+                                yAxisId='PCA2'
                             />
                             <Scatter
                                 isAnimationActive={false}
                                 cursor='pointer'
-                                onClick={setSelectedPoint}
+                                onClick={handlePointSelect}
                                 name='Inlier'
                                 data={data.filter(({outlier, novelty}) => !outlier && !novelty).map((d) => ({
                                     size: d.PCA1 === selectedPoint.PCA1 && d.PCA2 === selectedPoint.PCA2 ? LARGE_DOT_SIZE : SMALL_DOT_SIZE,
                                     ...d
                                 }))}
                                 fill={theme.primary}
+                                xAxisId='PCA1'
+                                yAxisId='PCA2'
                             />
+                            {refTopLeft && refBottomRight ? (
+                                <ReferenceArea
+                                    strokeOpacity={0.3}
+                                    x1={refTopLeft.x}
+                                    y1={refTopLeft.y}
+                                    x2={refBottomRight.x}
+                                    y2={refBottomRight.y}
+                                    xAxisId='PCA1'
+                                    yAxisId='PCA2'
+                                />
+                            ) : null}
                         </ScatterChart>
                     </ResponsiveContainer>
                 </Col>
@@ -173,7 +241,7 @@ const ScatterGraph = ({data}) => {
                                         width='100%'
                                     />
                                 </div>
-                            ))}{' '}
+                            ))}
                         </div>
                     ) : (
                         <div
