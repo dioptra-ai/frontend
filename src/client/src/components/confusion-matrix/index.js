7ea225e9d513cca3f967debad189f710847ed7e0
Ahmic, Adnan (FR - External) <adnan@ggstud.io>
2021-10-25 20:50:38 +0200

    Confusion matrix for document processing

--- src/client/src/components/confusion-matrix/index.js
+++ src/client/src/components/confusion-matrix/index.js
@@ -8,27 +8,42 @@ import ImageExamples from './image-examples';
 import TabularExamples from './tabular-examples';
 import useModel from 'customHooks/use-model';
 import DifferenceLabel from 'components/difference-labels';
+import Select from 'components/select';
+import Col from 'react-bootstrap/Col';
 
-const Table = ({data, referenceData, onCellClick, groundtruthClasses, predictionClasses}) => {
-
+const Table = ({
+    data,
+    referenceData,
+    onCellClick,
+    groundtruthClasses,
+    predictionClasses
+}) => {
     const classes = predictionClasses.sort().map((c) => ({
         Header: getName(c),
         accessor: c,
-        Cell: Object.assign(({value: cell = {}}) => {
-            const {value = 0, difference} = cell;
+        Cell: Object.assign(
+            ({value: cell = {}}) => {
+                const {value = 0, difference = 0} = cell;
 
-            return (
-                <DifferenceLabel value={`${(value * 100).toFixed(2)} %`} difference={(difference * 100).toFixed(2)} />
-            );
-        }, {displayName: 'Cell'})
+                return (
+                    <DifferenceLabel
+                        value={`${(value * 100).toFixed(2)} %`}
+                        difference={(difference * 100).toFixed(2)}
+                    />
+                );
+            },
+            {displayName: 'Cell'}
+        )
     }));
 
-    const columns = [{
-        Header: '',
-        accessor: 'groundtruth',
-        Cell: ({value}) => getName(value)
-    }, ...classes];
-
+    const columns = [
+        {
+            Header: '',
+            accessor: 'groundtruth',
+            Cell: ({value}) => getName(value)
+        },
+        ...classes
+    ];
 
     const rows = groundtruthClasses.sort().map((c) => {
         const filtered = data.filter((d) => d.groundtruth === c);
@@ -37,7 +52,9 @@ const Table = ({data, referenceData, onCellClick, groundtruthClasses, prediction
         const cells = {groundtruth: c};
 
         filtered.forEach((e) => {
-            const referenceDataForCell = referenceDataForClass.find((d) => d.prediction === e.prediction);
+            const referenceDataForCell = referenceDataForClass.find(
+                (d) => d.prediction === e.prediction
+            );
 
             cells[e.prediction] = {
                 value: e.distribution,
@@ -52,12 +69,12 @@ const Table = ({data, referenceData, onCellClick, groundtruthClasses, prediction
         <>
             <div className='position-relative' style={{marginLeft: '30px'}}>
                 <p className='text-secondary m-0 mb-2 text-center bold-text'>Prediction</p>
-                <MatrixTable columns={columns} data={rows} onCellClick={onCellClick}/>
+                <MatrixTable columns={columns} data={rows} onCellClick={onCellClick} />
                 <p
                     className='position-absolute text-secondary m-0 text-center bold-text'
                     style={{transform: 'rotate(-90deg)', top: '50%', left: '-70px'}}
                 >
-                   Ground Truth
+          Ground Truth
                 </p>
             </div>
         </>
@@ -77,6 +94,7 @@ const ConfusionMatrix = () => {
     const model = useModel();
     const allSqlFilters = useAllSqlFilters();
     const sqlFiltersWithModelTime = useAllSqlFilters({useReferenceRange: true});
+    const [iou, setIou] = useState(0.5);
 
     const getClasses = (data, key) => {
         const classes = [];
@@ -90,89 +108,106 @@ const ConfusionMatrix = () => {
         return classes;
     };
 
-    return (
+    const generateQuery = (filters) => {
+        switch (model.mlModelType) {
+        case 'DOCUMENT_PROCESSING':
+            return `SELECT
+                predictionTable.groundtruth,
+                predictionTable.prediction,
+                cast(predictionTable.c as FLOAT) / cast(groundTable.c as FLOAT) as distribution
+            FROM (
+                SELECT "bboxes.groundtruth" as groundtruth, 
+                    "bboxes.prediction" as prediction, 
+                    COUNT(*) AS c
+                FROM "dioptra-gt-combined-eventstream"
+                WHERE cast("bboxes.iou" as FLOAT) > ${iou} AND ${filters}
+                GROUP BY "bboxes.groundtruth", "bboxes.prediction"
+                ORDER BY "bboxes.groundtruth", "bboxes.groundtruth"
+            )  as predictionTable
+            LEFT JOIN (
+                SELECT "bboxes.groundtruth" as groundtruth,
+                    COUNT(*) AS c
+                FROM "dioptra-gt-combined-eventstream"
+                WHERE cast("bboxes.iou" as FLOAT) >= ${iou} AND ${filters}
+                GROUP BY "bboxes.groundtruth"
+            ) AS groundTable
+            ON groundTable.groundtruth = predictionTable.groundtruth`;
+        default:
+            return `SELECT
+                predictionTable.groundtruth,
+                predictionTable.prediction,
+                cast(predictionTable.c as FLOAT) / cast(groundTable.c as FLOAT) as distribution
+                FROM (
+                    SELECT groundtruth, prediction, COUNT(*) AS c
+                    FROM "dioptra-gt-combined-eventstream"
+                    WHERE ${filters}
+                    GROUP BY groundtruth, prediction
+                    ORDER BY groundtruth, prediction
+                )  as predictionTable
+                LEFT JOIN (
+                    SELECT groundtruth, COUNT(*) AS c
+                    FROM "dioptra-gt-combined-eventstream"
+                    WHERE ${filters}
+                    GROUP BY groundtruth
+                ) AS groundTable
+                ON groundTable.groundtruth = predictionTable.groundtruth`;
+        }
+    };
 
+    return (
         <div className='my-5'>
             <h3 className='text-dark bold-text fs-3 mb-3'>Confusion matrix</h3>
             <div className='border rounded p-3'>
+                {model.mlModelType === 'DOCUMENT_PROCESSING' ? (
+                    <Col lg={{span: 3, offset: 9}}>
+                        <Select
+                            options={[
+                                {name: 'iou=0.5', value: 0.5},
+                                {name: 'iou=0.75', value: 0.75},
+                                {name: 'iou=0.95', value: 0.95}
+                            ]}
+                            initialValue={iou}
+                            onChange={(val) => setIou(Number(val))}
+                        />
+                    </Col>
+                ) : null}
+
                 <TimeseriesQuery
                     defaultData={[]}
                     renderData={(data) => (
                         <TimeseriesQuery
                             defaultData={[]}
-                            renderData={(rangeData) => {
-
-                                // console.log(getClasses(data, 'groundtruth'), getClasses(data, 'prediction'));
-
-                                return (
-                                    <Table
-                                        data={data}
-                                        groundtruthClasses={getClasses(data, 'groundtruth')}
-                                        onCellClick={(prediction, groundtruth) => setSelectedCell({prediction, groundtruth})}
-                                        predictionClasses = {getClasses(data, 'prediction')}
-                                        referenceData={rangeData}
-                                    />
-                                );
-                            }}
-                            sql={sql`
-                        SELECT
-                        predictionTable.groundtruth,
-                        predictionTable.prediction,
-                        cast(predictionTable.c as FLOAT) / cast(groundTable.c as FLOAT) as distribution
-                        FROM (
-                            SELECT groundtruth, prediction, COUNT(*) AS c
-                            FROM "dioptra-gt-combined-eventstream"
-                            WHERE ${sqlFiltersWithModelTime}
-                            GROUP BY groundtruth, prediction
-                            ORDER BY groundtruth, prediction
-                        )  as predictionTable
-                        LEFT JOIN (
-                            SELECT groundtruth, COUNT(*) AS c
-                            FROM "dioptra-gt-combined-eventstream"
-                            WHERE ${sqlFiltersWithModelTime}
-                            GROUP BY groundtruth
-                        ) AS groundTable
-                        ON groundTable.groundtruth = predictionTable.groundtruth
-                    `}
+                            renderData={(rangeData) => (
+                                <Table
+                                    data={data}
+                                    groundtruthClasses={getClasses(data, 'groundtruth')}
+                                    onCellClick={(prediction, groundtruth) => setSelectedCell({prediction, groundtruth})
+                                    }
+                                    predictionClasses={getClasses(data, 'prediction')}
+                                    referenceData={rangeData}
+                                />
+                            )}
+                            sql={sql`${generateQuery(sqlFiltersWithModelTime)}`}
                         />
                     )}
-                    sql={sql`
-                        SELECT
-                        predictionTable.groundtruth,
-                        predictionTable.prediction,
-                        cast(predictionTable.c as FLOAT) / cast(groundTable.c as FLOAT) as distribution
-                        FROM (
-                            SELECT groundtruth, prediction, COUNT(*) AS c
-                            FROM "dioptra-gt-combined-eventstream"
-                            WHERE ${allSqlFilters}
-                            GROUP BY groundtruth, prediction
-                            ORDER BY groundtruth, prediction
-                        )  as predictionTable
-                        LEFT JOIN (
-                            SELECT groundtruth, COUNT(*) AS c
-                            FROM "dioptra-gt-combined-eventstream"
-                            WHERE ${allSqlFilters}
-                            GROUP BY groundtruth
-                        ) AS groundTable
-                        ON groundTable.groundtruth = predictionTable.groundtruth
-                    `}
+                    sql={sql`${generateQuery(allSqlFilters)}`}
                 />
-                {selectedCell && (
-                    model.mlModelType === 'IMAGE_CLASSIFIER' ? (
-                        <ImageExamples
-                            groundtruth={selectedCell.groundtruth}
-                            model={model}
-                            onClose={() => setSelectedCell(null)}
-                            prediction={selectedCell.prediction}
-                        />
-                    ) : model.mlModelType === 'TABULAR_CLASSIFIER' ? (
-                        <TabularExamples
-                            groundtruth={selectedCell.groundtruth}
-                            onClose={() => setSelectedCell(null)}
-                            prediction={selectedCell.prediction}
-                        />
-                    ) : null
-                )}
+                {selectedCell &&
+          (model.mlModelType === 'IMAGE_CLASSIFIER' ? (
+              <ImageExamples
+                  groundtruth={selectedCell.groundtruth}
+                  model={model}
+                  onClose={() => setSelectedCell(null)}
+                  prediction={selectedCell.prediction}
+                  iou={iou}
+              />
+          ) : model.mlModelType === 'TABULAR_CLASSIFIER' ? (
+              <TabularExamples
+                  groundtruth={selectedCell.groundtruth}
+                  onClose={() => setSelectedCell(null)}
+                  prediction={selectedCell.prediction}
+              />
+          ) : null)}
             </div>
         </div>
     );
