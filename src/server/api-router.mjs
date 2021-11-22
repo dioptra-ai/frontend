c5abd5bf224aaa6e73db32c0d6285b0c5422c152
oleh-dioptra <oleh@dioptra.ai>
2021-11-22 17:20:36 +0100

    [DEV-175] Add endpoints to receive KPI information from source

--- src/server/api-router.mjs
+++ src/server/api-router.mjs
@@ -1,14 +1,14 @@
 import express from 'express';
-const ApiRouter = express.Router();
-
-import UserRouter from './controllers/user.controller.mjs';
-import AuthRouter from './controllers/auth.controller.mjs';
-import MlModelRouter from './controllers/ml-model.mjs';
-import Timeseries from './controllers/timeseries.mjs';
-import Metrics from './controllers/metrics.mjs';
 import ApiKey from './controllers/api-key.mjs';
-import OrganizationRouter from './controllers/organization.mjs';
+import AuthRouter from './controllers/auth.controller.mjs';
+import Metrics from './controllers/metrics.mjs';
+import MlModelRouter from './controllers/ml-model.mjs';
 import OrganizationMembershipRouter from './controllers/organization-membership.mjs';
+import OrganizationRouter from './controllers/organization.mjs';
+import Timeseries from './controllers/timeseries.mjs';
+import UserRouter from './controllers/user.controller.mjs';
+
+const ApiRouter = express.Router();
 
 ApiRouter.use('/user', UserRouter);
 ApiRouter.use('/auth', AuthRouter);
--- src/server/controllers/metrics.mjs
+++ src/server/controllers/metrics.mjs
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import express from 'express';
 import fetch from 'node-fetch';
 import {isAuthenticated} from '../middleware/authentication.mjs';
@@ -7,21 +8,25 @@ const MetricsRouter = express.Router();
 MetricsRouter.all('*', isAuthenticated);
 
 MetricsRouter.post('/', isAuthenticated, async (req, res, next) => {
-
     try {
-        const metricsResponse = await fetch(`${process.env.METRICS_ENGINE_URL}/compute`, {
-            headers: {
-                'content-type': 'application/json;charset=UTF-8'
-            },
-            body: JSON.stringify(req.body),
-            method: 'post'
-        });
+        const metricsResponse = await fetch(
+            `${process.env.METRICS_ENGINE_URL}/compute`,
+            {
+                headers: {
+                    'content-type': 'application/json;charset=UTF-8'
+                },
+                body: JSON.stringify(req.body),
+                method: 'post'
+            }
+        );
 
         if (metricsResponse.status !== 200) {
             let errorMessage = 'Internal Server Error';
 
             try {
-                errorMessage = await metricsResponse.json().then((json) => json.error.message);
+                errorMessage = await metricsResponse
+                    .json()
+                    .then((json) => json.error.message);
             } catch {
                 errorMessage = metricsResponse.statusText;
             }
@@ -30,7 +35,58 @@ MetricsRouter.post('/', isAuthenticated, async (req, res, next) => {
         }
 
         metricsResponse.body.pipe(res);
+    } catch (e) {
+        next(e);
+    }
+});
 
+// eslint-disable-next-line no-unused-vars
+MetricsRouter.get('/source/list', async (req, res, next) => {
+    try {
+        await axios
+            .get(`${process.env.METRICS_ENGINE_URL}/kpi/source/list`)
+            .then((response) => {
+                res.status(response.status);
+                res.json(response.data);
+            });
+    } catch (e) {
+        next(e);
+    }
+});
+
+MetricsRouter.get('/integrations/:sourceName', async (req, res, next) => {
+    try {
+        const sourceName = req.params.sourceName;
+        const organization_id =
+            req.user.activeOrganizationmembership.organization._id;
+
+        await axios
+            .get(
+                `${process.env.METRICS_ENGINE_URL}/kpi/${sourceName}/queries?org_id=${organization_id}`
+            )
+            .then((response) => {
+                res.status(response.status);
+                res.json(response.data);
+            });
+    } catch (e) {
+        next(e);
+    }
+});
+
+MetricsRouter.get('/integrations/:sourceName/:queryId', async (req, res, next) => {
+    try {
+        const {sourceName, queryId} = req.params;
+        const organization_id =
+            req.user.activeOrganizationmembership.organization._id;
+
+        await axios
+            .get(
+                `${process.env.METRICS_ENGINE_URL}/kpi/${sourceName}/results/${queryId}?org_id=${organization_id}`
+            )
+            .then((response) => {
+                res.status(response.status);
+                res.json(response.data);
+            });
     } catch (e) {
         next(e);
     }
