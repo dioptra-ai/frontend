0ecda1cc90854a5efac6ecbf8fbe511a700b8a55
Jacques Arnoux <jacques@dioptra.ai>
2021-12-01 16:33:25 -0800

    added fake alert metrics

--- src/client/src/enums/comparators.js
+++ src/client/src/enums/comparators.js
@@ -1,9 +1,7 @@
 export const Comparators = {
-    IS_ABOVE: {value: 'IS_ABOVE', name: 'IS ABOVE'},
-    IS_BELOW: {value: 'IS_BELOW', name: 'IS BELOW'},
-    IS_OUTSIDE_RANGE: {value: 'IS_OUTSIDE_RANGE', name: 'IS OUTSIDE RANGE'},
-    IS_WITHIN_RANGE: {value: 'IS_WITHIN_RANGE', name: 'IS WITHIN RANGE'},
+    IS_ABOVE: {value: 'IS_ABOVE', name: '>'},
+    IS_BELOW: {value: 'IS_BELOW', name: '<'},
     HAS_NO_VALUE: {value: 'HAS_NO_VALUE', name: 'HAS NO VALUE'},
-    IS_ABOVE_OR_EQUAL: {value: 'IS_ABOVE_OR_EQUAL', name: 'IS ABOVE OR EQUAL'},
-    IS_BELOW_OR_EQUAL: {value: 'IS_BELOW_OR_EQUAL', name: 'IS BELOW OR EQUAL'}
+    IS_ABOVE_OR_EQUAL: {value: 'IS_ABOVE_OR_EQUAL', name: '>='},
+    IS_BELOW_OR_EQUAL: {value: 'IS_BELOW_OR_EQUAL', name: '<='}
 };
