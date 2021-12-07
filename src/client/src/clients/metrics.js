73cfe06e16e66c6224cdba51e29eb37bafbd9b88
Nithin Perumal <nperumal@Nithins-MacBook-Pro.local>
2021-12-06 18:00:37 -0800

    Added some boilerplate for clientside caching

--- src/client/src/clients/metrics.js
+++ src/client/src/clients/metrics.js
@@ -1,6 +1,37 @@
 import baseJSONClient from 'clients/base-json-client';
+import mem from 'mem';
+
+const jsonFetch = async (...args) => {
+    const res = await window.fetch(...args);
+
+    let responseBody = await res.text();
+
+    try {
+        responseBody = JSON.parse(responseBody);
+    } catch (e) {
+        console.warn(`Failed to JSON parse response: ${responseBody}`);
+    }
+
+    if (responseBody.error) {
+
+        throw new Error(responseBody.error);
+    } else if (res.ok) {
+
+        return responseBody;
+    } else {
+
+        throw new Error(responseBody || res.statusText);
+    }
+};
+const memoizedFetch = mem(jsonFetch, {
+    cacheKey: JSON.stringify,
+    maxAge: 1000 * 60 * 5 // 5 minutes
+});
 
 const metricsClient = (path, body) => {
+    if (path === '/compute') {
+
+    }
 
     return baseJSONClient(`/api/metrics/${path}`, {
         method: 'POST',
