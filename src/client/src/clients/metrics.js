93f53712e6b6c389129dc96e626057e9a0cf82ef
Nithin Perumal <nperumal@Nithins-MacBook-Pro.local>
2021-12-08 20:19:10 -0800

    Fixed calls not too parse too early

--- src/client/src/clients/metrics.js
+++ src/client/src/clients/metrics.js
@@ -3,12 +3,12 @@ import baseJSONClient from 'clients/base-json-client';
 // import {memoizedFetch} from './base-json-client';
 
 const metricsClient = (path, body, method = 'get', headers = {'content-type': 'application/json'}) => {
-    // console.log(`path: ${path}`);
-    // console.log(body);
+    console.log(`path: ${path}`);
+    console.log(body);
 
     return baseJSONClient(`/api/metrics/${path}`, { //baseJSONClient uses memoization logic when memoization=true
         method, headers,
-        body: JSON.stringify(body),
+        body,
         memoized: true
     });
 
