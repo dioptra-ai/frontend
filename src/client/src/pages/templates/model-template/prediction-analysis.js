67d506a269b9c40dfa3185f805f141747c3883ac
oleh-dioptra <94024812+oleh-dioptra@users.noreply.github.com>
2021-12-15 10:02:51 +0100

    Add box size analysis support (#280)

--- src/client/src/pages/templates/model-template/prediction-analysis.js
+++ src/client/src/pages/templates/model-template/prediction-analysis.js
@@ -111,6 +111,55 @@ const PredictionAnalysis = ({timeStore, filtersStore}) => {
                 </Row>
             </div>
 
+            <div className='my-3'>
+                <h3 className='text-dark bold-text fs-3 mb-3'>
+                    Bounding Box Size Analysis
+                </h3>
+                <Row className='my-3'>
+                    <Col className='d-flex' lg={4}>
+                        <Async
+                            refetchOnChanged={[allSqlFilters, timeGranularity]}
+                            renderData={(data) => (
+                                <BarGraph
+                                    bars={data.map(({name, value}) => ({
+                                        name,
+                                        value,
+                                        fill: getHexColor(value)
+                                    }))}
+                                    title='Bounding Box Size Distribution'
+                                    unit='%'
+                                    yAxisName='Percent'
+                                />
+                            )}
+                            fetchData={
+                                () => metricsClient('query/image-distribution',
+                                    {sql_filters: allSqlFilters, time_granularity: timeGranularity})
+                            }
+                        />
+                    </Col>
+                    <Col className='d-flex' lg={8}>
+                        <Async
+                            refetchOnChanged={[timeGranularity, allSqlFilters]}
+                            renderData={(data) => (
+                                <AreaGraph
+                                    dots={data}
+                                    title='Average'
+                                    xAxisDomain={timeStore.rangeMillisec}
+                                    xAxisName='Time'
+                                    yAxisName='Relative Coordinates (%)'
+                                    xDataKey='time'
+                                    yDataKey='value'
+                                />
+                            )}
+                            fetchData={
+                                () => metricsClient('query/image-distribution-average',
+                                    {sql_filters: allSqlFilters, time_granularity: timeGranularity})
+                            }
+                        />
+                    </Col>
+                </Row>
+            </div>
+
             {mlModelType === 'DOCUMENT_PROCESSING' ? (
                 <>
                     <div className='my-3'>
