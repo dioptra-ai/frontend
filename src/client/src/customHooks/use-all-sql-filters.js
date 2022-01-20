edf6520fcb76dfb6ba7c5ba11a716c37f2b30fe0
Nithin Perumal <nperumal@Nithins-MacBook-Pro.local>
2022-01-20 12:13:58 -0800

    Added logic fixes to use-all-sql-filters

--- src/client/src/customHooks/use-all-sql-filters.js
+++ src/client/src/customHooks/use-all-sql-filters.js
@@ -7,7 +7,7 @@ const {filtersStore, timeStore, modelStore} = stores;
 const useAllSqlFilters = ({useReferenceRange = false, __REMOVE_ME__excludeOrgId} = {}) => {
     const params = useParams();
     const activeModelId = params._id;
-    const {benchmarkSet, benchmarkModel, benchmarkMlModelVersion, referencePeriod} = modelStore.getModelById(activeModelId);
+    const {mlModelId, benchmarkSet, benchmarkModel, benchmarkMlModelVersion, referencePeriod} = modelStore.getModelById(activeModelId);
     const allFilters = [
         __REMOVE_ME__excludeOrgId ? filtersStore.__RENAME_ME__sqlFilters :
             filtersStore.sqlFilters
@@ -30,22 +30,30 @@ const useAllSqlFilters = ({useReferenceRange = false, __REMOVE_ME__excludeOrgId}
     // } else {
     //     allFilters.push(timeStore.sqlTimeFilter);
     // }
-    if (benchmarkSet) {
-        if (benchmarkModel) {
-            allFilters.push(`model_id='${benchmarkModel}'`);
+    if (useReferenceRange) {
+        if (benchmarkSet) {
+            if (benchmarkModel) {
+                allFilters.push(`model_id='${benchmarkModel}'`);
 
-            if (useReferenceRange && referencePeriod) {
-                allFilters.push(`"__time" >= TIME_PARSE('${referencePeriod.start}') AND "__time" < TIME_PARSE('${referencePeriod.end}')`);
+                if (referencePeriod) {
+                    allFilters.push(`"__time" >= TIME_PARSE('${referencePeriod.start}') AND "__time" < TIME_PARSE('${referencePeriod.end}')`);
 
-                if (benchmarkMlModelVersion) {
-                    allFilters.push(`model_version='${benchmarkMlModelVersion}'`);
+                    if (benchmarkMlModelVersion) {
+                        allFilters.push(`model_version='${benchmarkMlModelVersion}'`);
+                    }
                 }
+            } else { // Its an offline dataset
+                console.log('offline dataset');
             }
-        } else { // Its an offline dataset
-            console.log('offline dataset');
+        } else {
+            allFilters.push(timeStore.sqlTimeFilter);
         }
-    } else {
+    } else { // Not a benchmark query
+        if (mlModelId) {
+            allFilters.push(`model_id='${mlModelId}'`);
+        }
         allFilters.push(timeStore.sqlTimeFilter);
+
     }
 
     return allFilters.join(' AND ');
