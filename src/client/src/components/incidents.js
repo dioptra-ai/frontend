03e964a434aa269453a6bf42fac68448f94de59b
oleh-dioptra <oleh@dioptra.ai>
2022-01-21 14:28:38 +0100

    Improve alerts and incidents

--- src/client/src/components/incidents.js
+++ src/client/src/components/incidents.js
@@ -1,8 +1,8 @@
 import baseJSONClient from 'clients/base-json-client';
 import PropTypes from 'prop-types';
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import Button from 'react-bootstrap/Button';
-import { IconNames } from '../constants';
+import {IconNames} from '../constants';
 import useModal from '../customHooks/useModal';
 import FontIcon from './font-icon';
 import Modal from './modal';
@@ -23,9 +23,9 @@ const IncidentRow = ({
             } incident-row`}
         >
             {!resolved && (
-                <label className="checkbox">
+                <label className='checkbox'>
                     <input
-                        type="checkbox"
+                        type='checkbox'
                         checked={checked}
                         onChange={(event) => {
                             selectCallback(id, event.target.checked);
@@ -47,12 +47,12 @@ const IncidentRow = ({
                 {name}
             </span>
             <Button
-                className="text-white btn-incident p-0 fs-6"
+                className='text-white btn-incident p-0 fs-6'
                 variant={resolved ? 'success' : 'warning'}
             >
                 {resolved ? 'Resolved' : 'Open'}
             </Button>
-            {!isMainRow && <div className="mx-2" />}
+            {!isMainRow && <div className='mx-2' />}
         </div>
     );
 };
@@ -79,6 +79,7 @@ const Incidents = ({incidents, refreshCallback}) => {
                     .map((incident) => incident.alert_id)
                     .sort()
             );
+
             setAllEventsSelected(
                 JSON.stringify(selectedEventIds.sort()) === openedIncedents
             );
@@ -103,11 +104,11 @@ const Incidents = ({incidents, refreshCallback}) => {
 
     const handleSelectAllEvents = (checked) => {
         setSelectedEventIds(
-            checked
-                ? incidents
-                      .filter((incident) => incident.state === 'open')
-                      .map((incident) => incident.alert_id)
-                : []
+            checked ?
+                incidents
+                    .filter((incident) => incident.state === 'open')
+                    .map((incident) => incident.alert_id) :
+                []
         );
     };
 
@@ -124,21 +125,21 @@ const Incidents = ({incidents, refreshCallback}) => {
     };
 
     return (
-        <div className="incidents">
-            <div className="header mb-3">
-                <p className="bold-text fs-3 text-dark">Incidents</p>
-                <div className="d-flex gap-2">
+        <div className='incidents'>
+            <div className='header mb-3'>
+                <p className='bold-text fs-3 text-dark'>Incidents</p>
+                <div className='d-flex gap-2'>
                     <Button
                         style={{width: 40}}
-                        className="text-white bold-text"
-                        variant="secondary"
+                        className='text-white bold-text'
+                        variant='secondary'
                         onClick={() => refreshCallback(false)}
                     >
-                        <FontIcon className="text-white" icon="Refresh" size={14} />
+                        <FontIcon className='text-white' icon='Refresh' size={14} />
                     </Button>
                     <Button
-                        className="text-white bold-text fs-6"
-                        variant="primary"
+                        className='text-white bold-text fs-6'
+                        variant='primary'
                         onClick={() => setResolveIncidentModal(true)}
                         disabled={selectedEventIds.length === 0}
                     >
@@ -146,18 +147,17 @@ const Incidents = ({incidents, refreshCallback}) => {
                     </Button>
                 </div>
             </div>
-            <div className="border rounded px-3">
-                <div className="table-row py-4 text-secondary bold-text">
-                    <div className="flex-grow-1">
-                        <label className="checkbox">
+            <div className='border rounded px-3'>
+                <div className='table-row py-4 text-secondary bold-text'>
+                    <div className='flex-grow-1'>
+                        <label className='checkbox'>
                             <input
-                                type="checkbox"
+                                type='checkbox'
                                 checked={allEventsSelected}
-                                onChange={(event) =>
-                                    handleSelectAllEvents(event.target.checked)
+                                onChange={(event) => handleSelectAllEvents(event.target.checked)
                                 }
                             />
-                            <span className="fs-6">Incidents Name</span>
+                            <span className='fs-6'>Incidents Name</span>
                         </label>
                     </div>
                 </div>
@@ -181,21 +181,21 @@ const Incidents = ({incidents, refreshCallback}) => {
                 isOpen={resolveIncidentModal}
                 onClose={() => setResolveIncidentModal(false)}
             >
-                <p className="text-dark bold-text fs-4 my-5 px-3 text-center">
+                <p className='text-dark bold-text fs-4 my-5 px-3 text-center'>
                     Are you sure you want to resolve selected incidents?
                 </p>
-                <div className="d-flex justify-content-center border-top pt-4">
+                <div className='d-flex justify-content-center border-top pt-4'>
                     <Button
-                        className="text-white mx-2 py-2 px-5 bold-text fs-6"
+                        className='text-white mx-2 py-2 px-5 bold-text fs-6'
                         onClick={() => handleResolveEvents()}
-                        variant="primary"
+                        variant='primary'
                     >
                         RESOLVE
                     </Button>
                     <Button
-                        className="text-secondary mx-2 py-2 px-5 bold-text fs-6"
+                        className='text-secondary mx-2 py-2 px-5 bold-text fs-6'
                         onClick={() => setResolveIncidentModal(false)}
-                        variant="light"
+                        variant='light'
                     >
                         CANCEL
                     </Button>
