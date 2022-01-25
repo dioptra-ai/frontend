d6f9e8c09368c1d2b37e38db4bd6fca2d1ed3935
Pierre <pierre@dioptra.ai>
2022-01-25 14:09:35 +0100

    Added cluster analysis for benchnark

--- src/client/src/pages/templates/model-template/feature-analysis/index.js
+++ src/client/src/pages/templates/model-template/feature-analysis/index.js
@@ -2,16 +2,21 @@ import {setupComponent} from 'helpers/component-helper';
 import useModel from 'customHooks/use-model';
 import FeatureIntegrityTable from './feature-integrity';
 import FeatureAnalysisImages from './feature-analysis-images';
+import QAPerfAnalysis from 'pages/templates/model-template/qa-perf-analysis';
 
-const FeatureAnalysis = () => {
+const FeatureAnalysis = (timeStore) => {
     const model = useModel();
 
     switch (model.mlModelType) {
     case 'IMAGE_CLASSIFIER':
     case 'DOCUMENT_PROCESSING':
     case 'UNSUPERVISED_OBJECT_DETECTION':
+        if (timeStore.enabled) {
+            return <FeatureAnalysisImages/>;
+        }
 
-        return <FeatureAnalysisImages/>;
+        return <QAPerfAnalysis/>;
+
     case 'TABULAR_CLASSIFIER':
 
         return <FeatureIntegrityTable/>;
