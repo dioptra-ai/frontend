df9657b0624cf294ab6e2aae381a2d0989e38c3a
Jacques Arnoux <jacques@dioptra.ai>
2022-01-24 19:07:11 -0800

    quick checkpoint

--- src/client/src/pages/experimentations.js
+++ src/client/src/pages/experimentations.js
@@ -1,23 +1,15 @@
+import PropTypes from 'prop-types';
 import Table from 'react-bootstrap/Table';
-<<<<<<< HEAD
 import {useHistory} from 'react-router-dom';
-=======
->>>>>>> 595dc5d (WIP)
 
 import Async from 'components/async';
 import GeneralSearchBar from './templates/general-search-bar';
 import metricsClient from 'clients/metrics';
-<<<<<<< HEAD
 import {setupComponent} from 'helpers/component-helper';
 
 
 const Experimentations = ({filtersStore, modelStore}) => {
     const history = useHistory();
-=======
-
-
-const Experimentations = () => {
->>>>>>> 595dc5d (WIP)
 
     return (
         <>
@@ -32,20 +24,14 @@ const Experimentations = () => {
                         <Table className='models-table'>
                             <thead className='align-middle text-secondary'>
                                 <tr className='border-0 border-bottom border-mercury'>
-<<<<<<< HEAD
                                     <th>Benchmark ID</th>
                                     <th>Dataset ID</th>
                                     <th>Model</th>
-=======
-                                    <th>Dataset ID</th>
-                                    <th>Model ID</th>
->>>>>>> 595dc5d (WIP)
                                     <th>Model Version</th>
                                     <th>Started At</th>
                                 </tr>
                             </thead>
                             <tbody>
-<<<<<<< HEAD
                                 {benchmarks.map(({benchmark_id, dataset_id, model_id, model_version, started_at}, i) => (
                                     <tr className='cursor-pointer' onClick={() => {
                                         filtersStore.filters = [{
@@ -71,12 +57,6 @@ const Experimentations = () => {
                                         <td>{benchmark_id}</td>
                                         <td>{dataset_id}</td>
                                         <td>{modelStore.models.find((m) => m.mlModelId === model_id)?.name || '<unknown>'}</td>
-=======
-                                {benchmarks.map(({model_id, model_version, started_at}, i) => (
-                                    <tr key={i}>
-                                        <td></td>
-                                        <td>{model_id}</td>
->>>>>>> 595dc5d (WIP)
                                         <td>{model_version}</td>
                                         <td>{new Date(started_at).toLocaleString()}</td>
                                     </tr>
@@ -90,8 +70,9 @@ const Experimentations = () => {
     );
 };
 
-<<<<<<< HEAD
+Experimentations.propTypes = {
+    filtersStore: PropTypes.object.isRequired,
+    modelStore: PropTypes.object.isRequired
+};
+
 export default setupComponent(Experimentations);
-=======
-export default Experimentations;
->>>>>>> 595dc5d (WIP)
