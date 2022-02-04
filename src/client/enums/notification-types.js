5f222da621f4ac2232ea689c97c86e36366c8e47
oleh-dioptra <oleh@dioptra.ai>
2022-02-04 14:59:06 +0100

    Add support for alerts notification channels

--- src/client/enums/notification-types.js
+++ src/client/enums/notification-types.js
@@ -1,5 +1,3 @@
 export const NotificationTypes = {
-    EMAIL: {value: 'EMAIL', name: 'Email'},
-    SLACK: {value: 'SLACK', name: 'Slack'},
-    PAGER_DUTY: {value: 'PAGER_DUTY', name: 'PagerDuty'}
+    SLACK: {value: 'SLACK', name: 'Slack'}
 };
