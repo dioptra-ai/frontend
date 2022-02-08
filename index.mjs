16faaefe2d215c0c678def7e002f543df03a24de
Jacques Arnoux <jacques@dioptra.ai>
2022-02-07 17:15:36 -0800

    fixed authentication

--- index.mjs
+++ index.mjs
@@ -6,23 +6,22 @@ import {} from 'dotenv/config';
 import {sessionHandler, userAuth} from './src/server/middleware/authentication.mjs';
 import ApiRouter from './src/server/api-router.mjs';
 import jsonError from './src/server/middleware/json-error.mjs';
-import {isAuthenticated} from '/src/servermiddleware/authentication.mjs';
+import {isAuthenticated} from './src/server/middleware/authentication.mjs';
 import './src/server/models/index.mjs';
 
 const app = express();
 const basePath = dirname(fileURLToPath(import.meta.url));
+
+userAuth(passport);
+app.use(sessionHandler);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/documentation', isAuthenticated);
 app.use(express.static(join(basePath, 'build')));
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-
-userAuth(passport);
-
-app.use(sessionHandler);
-app.use(passport.initialize());
-app.use(passport.session());
 
 // Register all controller routes to /api/ basepath
 app.use('/api', ApiRouter);
