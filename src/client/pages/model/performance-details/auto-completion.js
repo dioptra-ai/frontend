9181ac0e900a2a9bbbf499247372968804627c1b
Jacques Arnoux <jacques@dioptra.ai>
2022-02-11 17:21:46 -0800

    added perf cluster analysis to autocomplete

--- src/client/pages/model/performance-details/auto-completion.js
+++ src/client/pages/model/performance-details/auto-completion.js
@@ -1,9 +1,11 @@
+import PerformanceClustersAnalysis from 'pages/common/performance-clusters-analysis';
 import Segmentation from 'pages/common/segmentation';
 
 const PerformanceDetails = () => {
 
     return (
         <div className='pb-5'>
+            <PerformanceClustersAnalysis/>
             <Segmentation />
         </div>
     );
