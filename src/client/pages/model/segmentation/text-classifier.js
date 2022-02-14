9687ced4c32f69ec6e383eb98f66715f0717e65e
Pierre <pierre@dioptra.ai>
2022-02-14 15:53:50 +0100

    Hiding clustering in text class for now

--- src/client/pages/model/segmentation/text-classifier.js
+++ src/client/pages/model/segmentation/text-classifier.js
@@ -1,11 +1,9 @@
-import PerformanceClustersAnalysis from 'pages/common/performance-clusters-analysis';
 import Segmentation from 'pages/common/segmentation';
 
 const TextClassifier = () => {
 
     return (
         <div className='pb-5'>
-            <PerformanceClustersAnalysis/>
             <Segmentation />
         </div>
     );
