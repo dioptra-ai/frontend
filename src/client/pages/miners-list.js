c797eed9594d1abb8a1af80cc2918c0ddbaccdf2
Oleh Oleksiuk <oleh@dioptra.ai>
2022-03-31 14:59:26 +0200

    Improvements for miner status update

--- src/client/pages/miners-list.js
+++ src/client/pages/miners-list.js
@@ -6,44 +6,44 @@ import {useEffect, useState} from 'react';
 import Table from 'react-bootstrap/Table';
 import {IoDownloadOutline} from 'react-icons/io5';
 import {BarLoader} from 'react-spinners';
+import baseJSONClient from 'clients/base-json-client';
 
 const MinersList = () => {
     const [miners, setMiners] = useState();
 
     const downloadDatapoints = async (minerId) => {
-        return await metricsClient(`/miner/datapoints?id=${minerId}`, null, 'get');
+        return await metricsClient(`miner/datapoints?id=${minerId}`, null, 'get');
     };
 
     useEffect(() => {
         const interval = setInterval(() => {
-            fetchMiners();
+            fetchMiners().then((miners) => setMiners(miners));
         }, 5000);
-
 
         return () => clearInterval(interval);
     }, []);
 
     useEffect(() => {
-        fetchMiners();
+        fetchMiners().then((miners) => setMiners(miners));
     }, []);
 
-    const fetchMiners = () => metricsClient('miners', null, 'get').then((miners) => setMiners(miners));
+    const fetchMiners = () => baseJSONClient(`/api/metrics/miners`, {memoized: false});
 
     return (
         <Menu>
             <GeneralSearchBar shouldShowOnlySearchInput />
-            <div className='p-4 mt-5'>
-                <div className='d-flex justify-content-between'>
-                    <span className='h2 fs-1 text-dark bold-text'>Miners</span>
+            <div className="p-4 mt-5">
+                <div className="d-flex justify-content-between">
+                    <span className="h2 fs-1 text-dark bold-text">Miners</span>
                 </div>
-                <Table className='models-table mt-3'>
-                    <thead className='align-middle text-secondary'>
-                        <tr className='border-0 border-bottom border-mercury'>
-                            <th className='text-secondary'>Miner ID</th>
-                            <th className='text-secondary'>Size</th>
-                            <th className='text-secondary'>Created At</th>
-                            <th className='text-secondary'>Status</th>
-                            <th className='text-secondary d-flex justify-content-end'>
+                <Table className="models-table mt-3">
+                    <thead className="align-middle text-secondary">
+                        <tr className="border-0 border-bottom border-mercury">
+                            <th className="text-secondary">Miner ID</th>
+                            <th className="text-secondary">Size</th>
+                            <th className="text-secondary">Created At</th>
+                            <th className="text-secondary">Status</th>
+                            <th className="text-secondary d-flex justify-content-end">
                                 Download Datapoints
                             </th>
                         </tr>
@@ -62,21 +62,33 @@ const MinersList = () => {
                                         </td>
                                         <td>{miner.status}</td>
                                         <td
-                                            className='d-flex justify-content-end'
+                                            className="d-flex justify-content-end"
                                             style={{height: 50}}
                                         >
-                                            <div className='d-flex justify-content-center align-content-center align-items-center'>
+                                            <div className="d-flex justify-content-center align-content-center align-items-center">
                                                 {miner.status === 'pending' ? (
                                                     <BarLoader loading size={40} />
                                                 ) : (
-                                                    <IoDownloadOutline
-                                                        className='fs-3 cursor-pointer'
-                                                        onClick={async () => {
-                                                            const datapoints = await downloadDatapoints(miner._id);
-                                                            saveAs(new Blob([datapoints], {type: 'application/json;charset=utf-8'}), 'datapoints.json');
-                                                        }
-                                                        }
-                                                    />
+                                                    miner.status !== 'error' && (
+                                                        <IoDownloadOutline
+                                                            className="fs-3 cursor-pointer"
+                                                            onClick={async () => {
+                                                                const datapoints =
+                                                                    await downloadDatapoints(
+                                                                        miner._id
+                                                                    );
+                                                                saveAs(
+                                                                    new Blob(
+                                                                        [datapoints],
+                                                                        {
+                                                                            type: 'application/json;charset=utf-8'
+                                                                        }
+                                                                    ),
+                                                                    'datapoints.json'
+                                                                );
+                                                            }}
+                                                        />
+                                                    )
                                                 )}
                                             </div>
                                         </td>
