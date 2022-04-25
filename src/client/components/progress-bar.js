85218d4a74c9f4dd973ca9ba6f6bfad409b6e6e0
Jacques Arnoux <jacques@dioptra.ai>
2022-04-24 20:24:11 -0700

    fixed performance per class display

--- src/client/components/progress-bar.js
+++ src/client/components/progress-bar.js
@@ -1,6 +1,8 @@
 import PropTypes from 'prop-types';
 
 const ProgressBar = ({completed}) => {
+    const displayCompleted = Number(completed).toFixed(2);
+
     return (
         <div
             className='bg-light-blue cursor-pointer'
@@ -8,9 +10,9 @@ const ProgressBar = ({completed}) => {
         >
             <div
                 className='bg-primary position-relative'
-                style={{height: '100%', width: `${completed}%`}}
+                style={{height: '100%', width: `${displayCompleted}%`}}
             >
-                <span className='position-absolute text-secondary fs-6' style={{right: 0, top: -20}}>{completed}%</span>
+                <span className='position-absolute text-secondary fs-6' style={{right: 0, top: -20}}>{displayCompleted}%</span>
             </div>
         </div>
     );
