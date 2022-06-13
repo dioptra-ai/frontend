abeb2dd613ac817ecbe165d752ab5b0a797bc920
Jacques Arnoux <jacques@dioptra.ai>
2022-06-13 00:17:45 -0700

    added email verification

--- src/server/models/user.mjs
+++ src/server/models/user.mjs
@@ -1,3 +1,4 @@
+import fetch from 'node-fetch';
 import mongoose from 'mongoose';
 import bcrypt from 'bcrypt';
 import autopopulate from 'mongoose-autopopulate';
@@ -67,6 +68,34 @@ userSchema.statics.createAsMemberOf = async (userProps, organization) => {
 
     return newUser.save();
 };
+
+userSchema.pre('save', async function () {
+
+    if (this.isNew || this.isModified('username')) { // eslint-disable-line no-invalid-this
+        let status = 'valid';
+
+        try {
+            const response = await fetch(`https://api.hunter.io/v2/email-verifier?email=${this['username']}&api_key=29073794876a97a92ba2b8a1b45291305a2350af`); // eslint-disable-line no-invalid-this
+            const jsonResponse = await response.json();
+
+            status = jsonResponse['data']['status'];
+        } catch (e) {
+            console.warn(`API Error when verifying email address: ${e.message}`);
+        }
+
+        // see: https://hunter.io/api-documentation/v2#email-verifier
+        // "valid": the email address is valid.
+        // "invalid": the email address is not valid.
+        // "accept_all": the email address is valid but any email address is accepted by the server.
+        // "webmail": the email address comes from an email service provider such as Gmail or Outlook.
+        // "disposable": the email address comes from a disposable email service provider.
+        // "unknown": we failed to verify the email address.
+        if (status === 'invalid' || status === 'disposable') {
+
+            throw new Error('We could not verify your email. Please use a valid work email.');
+        }
+    }
+});
 
 const User = mongoose.model('User', userSchema);
 
