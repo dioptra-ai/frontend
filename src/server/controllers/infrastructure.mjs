bbe98f075aec6eefc683a49ec05717602b3b35e2
Jacques Arnoux <jacques@dioptra.ai>
2022-10-26 15:35:20 -0700

    added local data upload

---
+++ src/server/controllers/infrastructure.mjs
@@ -0,0 +1,39 @@
+import {v4 as uuidv4} from 'uuid';
+import express from 'express';
+import {GetObjectCommand, PutObjectCommand, S3Client} from '@aws-sdk/client-s3';
+import {getSignedUrl} from '@aws-sdk/s3-request-presigner';
+
+import {isAuthenticated} from '../middleware/authentication.mjs';
+
+const {AWS_S3_CUSTOMER_BUCKET, AWS_S3_CUSTOMER_UPLOAD_EXPIRATION_SECONDS = 300, ENVIRONMENT} = process.env;
+const s3 = new S3Client();
+
+const InfrastructureRouter = express.Router();
+
+InfrastructureRouter.all('*', isAuthenticated);
+
+InfrastructureRouter.get('/sign-s3-url-pair', async (req, res, next) => {
+    try {
+        const key = `${ENVIRONMENT}/${req.user.activeOrganizationMembership.organization._id}/${uuidv4()}.ndjson`;
+        const [put, get] = await Promise.all([
+            getSignedUrl(s3, new PutObjectCommand({
+                Bucket: AWS_S3_CUSTOMER_BUCKET,
+                Key: key
+            }), {
+                expiresIn: AWS_S3_CUSTOMER_UPLOAD_EXPIRATION_SECONDS
+            }),
+            getSignedUrl(s3, new GetObjectCommand({
+                Bucket: AWS_S3_CUSTOMER_BUCKET,
+                Key: key
+            }), {
+                expiresIn: AWS_S3_CUSTOMER_UPLOAD_EXPIRATION_SECONDS
+            })
+        ]);
+
+        res.json({put, get});
+    } catch (e) {
+        next(e);
+    }
+});
+
+export default InfrastructureRouter;
