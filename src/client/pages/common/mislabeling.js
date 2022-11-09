c9719ea0dc92c29e48d905bdec56147462aa2214
Jacques Arnoux <jacques@dioptra.ai>
2022-11-08 21:44:41 -0800

    added action buttons to each row in mislabeling

--- src/client/pages/common/mislabeling.js
+++ src/client/pages/common/mislabeling.js
@@ -21,53 +21,9 @@ const Mislabeling = ({userStore}) => {
     const allFilters = useAllFilters();
     const [exampleUUIDInModal, setExampleUUIDInModal] = useState(null);
     const [selectedRows, setSelectedRows] = useState([]);
-    const limit = 1000;
 
     return (
         <>
-            <div className='text-dark m-3 bold-text d-flex justify-content-between'>
-                <div>
-                    Total: {selectedRows.length >= limit ? `${limit.toLocaleString()}+` : selectedRows.length.toLocaleString()}
-                </div>
-                <div className='d-flex'>
-                    <OverlayTrigger overlay={<Tooltip>Add {selectedRows.length} to Data Cart</Tooltip>}>
-                        <button
-                            disabled={!selectedRows.length}
-                            className='d-flex text-dark border-0 bg-transparent click-down fs-2' onClick={() => {
-
-                                userStore.tryUpdate({
-                                    cart: userStore.userData.cart.concat(...selectedRows.map(({uuid}) => uuid))
-                                });
-                            }}>
-                            <BsCartPlus className='fs-2 ps-2 cursor-pointer' />
-                        </button>
-                    </OverlayTrigger>
-                    <OverlayTrigger overlay={<Tooltip>Accept {selectedRows.length} Suggested Label(s)</Tooltip>}>
-                        <button
-                            disabled={!selectedRows.length}
-                            className='d-flex text-dark border-0 bg-transparent click-down fs-2'
-                        >
-                            <BsPatchCheck className='fs-2 ps-2 cursor-pointer' />
-                        </button>
-                    </OverlayTrigger>
-                    <OverlayTrigger overlay={<Tooltip>Reject {selectedRows.length} Suggested Label(s)</Tooltip>}>
-                        <button
-                            disabled={!selectedRows.length}
-                            className='d-flex text-dark border-0 bg-transparent click-down fs-2'
-                        >
-                            <BsHandThumbsDown className='fs-2 ps-2 cursor-pointer' />
-                        </button>
-                    </OverlayTrigger>
-                    <OverlayTrigger overlay={<Tooltip>Delete {selectedRows.length} Datapoint(s)</Tooltip>}>
-                        <button
-                            disabled={!selectedRows.length}
-                            className='d-flex text-dark border-0 bg-transparent click-down fs-2'
-                        >
-                            <AiOutlineDelete className='fs-2 ps-2 cursor-pointer' />
-                        </button>
-                    </OverlayTrigger>
-                </div>
-            </div>
             <div className='my-3'>
                 <Async
                     fetchData={() => metricsClient('mislabeling-score', {
@@ -75,56 +31,143 @@ const Mislabeling = ({userStore}) => {
                     })}
                     refetchOnChanged={[JSON.stringify(allFilters)]}
                     renderData={(datapoints) => (
-                        <Table
-                            onSelectedRowsChange={setSelectedRows}
-                            columns={[{
-                                Header: 'Preview',
-                                accessor: 'uuid',
-                                disableSortBy: true,
-                                Cell: ({value}) => ( // eslint-disable-line react/prop-types
-                                    <div className='d-flex align-items-center justify-content-center'>
-                                        <div className='my-2 cursor-pointer' onClick={() => setExampleUUIDInModal(value)}>
-                                            <Async
-                                                fetchData={() => metricsClient('select', {
-                                                    select: '"uuid", "image_metadata", "text_metadata", "video_metadata"',
-                                                    filters: [{
-                                                        left: 'uuid',
-                                                        op: '==',
-                                                        right: value
-                                                    }]
-                                                })}
-                                                renderData={(datapoints) => {
-                                                    const {image_metadata, text_metadata, video_metadata} = datapoints[0];
+                        <>
 
-                                                    if (image_metadata || video_metadata) {
+                            <div className='text-dark m-3 bold-text d-flex justify-content-between'>
+                                <div>
+                                    Total: {datapoints.length.toLocaleString()}
+                                </div>
+                                <div className='d-flex'>
+                                    <OverlayTrigger overlay={<Tooltip>Add {selectedRows.length} to Data Cart</Tooltip>}>
+                                        <button
+                                            disabled={!selectedRows.length}
+                                            className='d-flex text-dark border-0 bg-transparent click-down fs-2' onClick={() => {
 
-                                                        return <PreviewImage datapoint={datapoints[0]} maxHeight={100}/>;
-                                                    } else if (text_metadata) {
+                                                userStore.tryUpdate({
+                                                    cart: userStore.userData.cart.concat(...selectedRows.map(({uuid}) => uuid))
+                                                });
+                                            }}>
+                                            <BsCartPlus className='fs-2 ps-2 cursor-pointer' />
+                                        </button>
+                                    </OverlayTrigger>
+                                    <OverlayTrigger overlay={<Tooltip>Accept {selectedRows.length} Suggested Label(s)</Tooltip>}>
+                                        <button
+                                            disabled={!selectedRows.length}
+                                            className='d-flex text-dark border-0 bg-transparent click-down fs-2'
+                                        >
+                                            <BsPatchCheck className='fs-2 ps-2 cursor-pointer' />
+                                        </button>
+                                    </OverlayTrigger>
+                                    <OverlayTrigger overlay={<Tooltip>Reject {selectedRows.length} Suggested Label(s)</Tooltip>}>
+                                        <button
+                                            disabled={!selectedRows.length}
+                                            className='d-flex text-dark border-0 bg-transparent click-down fs-2'
+                                        >
+                                            <BsHandThumbsDown className='fs-2 ps-2 cursor-pointer' />
+                                        </button>
+                                    </OverlayTrigger>
+                                    <OverlayTrigger overlay={<Tooltip>Delete {selectedRows.length} Datapoint(s)</Tooltip>}>
+                                        <button
+                                            disabled={!selectedRows.length}
+                                            className='d-flex text-dark border-0 bg-transparent click-down fs-2'
+                                        >
+                                            <AiOutlineDelete className='fs-2 ps-2 cursor-pointer' />
+                                        </button>
+                                    </OverlayTrigger>
+                                </div>
+                            </div>
+                            <Table
+                                onSelectedRowsChange={setSelectedRows}
+                                columns={[{
+                                    Header: 'Preview',
+                                    accessor: 'uuid',
+                                    disableSortBy: true,
+                                    Cell: ({value}) => ( // eslint-disable-line react/prop-types
+                                        <div className='d-flex align-items-center justify-content-center'>
+                                            <div className='my-2 cursor-pointer' onClick={() => setExampleUUIDInModal(value)}>
+                                                <Async
+                                                    fetchData={() => metricsClient('select', {
+                                                        select: '"uuid", "image_metadata", "text_metadata", "video_metadata"',
+                                                        filters: [{
+                                                            left: 'uuid',
+                                                            op: '==',
+                                                            right: value
+                                                        }]
+                                                    })}
+                                                    renderData={(datapoints) => {
+                                                        const {image_metadata, text_metadata, video_metadata} = datapoints[0];
 
-                                                        return <PreviewTextClassification sample={datapoints[0]}/>;
-                                                    } else {
+                                                        if (image_metadata || video_metadata) {
 
-                                                        return <div>N/A</div>;
-                                                    }
-                                                }}
-                                                refetchOnChanged={[value]}
-                                            />
+                                                            return <PreviewImage datapoint={datapoints[0]} maxHeight={100}/>;
+                                                        } else if (text_metadata) {
+
+                                                            return <PreviewTextClassification sample={datapoints[0]}/>;
+                                                        } else {
+
+                                                            return <div>N/A</div>;
+                                                        }
+                                                    }}
+                                                    refetchOnChanged={[value]}
+                                                />
+                                            </div>
                                         </div>
-                                    </div>
-                                )
-                            }, {
-                                Header: 'Ground Truth Label',
-                                accessor: 'groundtruth'
-                            }, {
-                                Header: 'Suggested Label',
-                                accessor: 'prediction'
-                            }, {
-                                Header: 'Mislabeling Score',
-                                accessor: 'mislabeling_score',
-                                sortType: 'basic'
-                            }]}
-                            data={datapoints}
-                        />
+                                    )
+                                }, {
+                                    Header: 'Ground Truth Label',
+                                    accessor: 'groundtruth'
+                                }, {
+                                    Header: 'Suggested Label',
+                                    accessor: 'prediction'
+                                }, {
+                                    Header: 'Mislabeling Score',
+                                    accessor: 'mislabeling_score',
+                                    sortType: 'basic'
+                                }, {
+                                    Header: '',
+                                    disableSortBy: true,
+                                    id: 'actions',
+                                    Cell: ({row}) => ( // eslint-disable-line react/prop-types
+                                        <div className='d-flex justify-content-end'>
+                                            <OverlayTrigger overlay={<Tooltip>Add to Data Cart</Tooltip>}>
+                                                <button
+                                                    className='d-flex text-dark border-0 bg-transparent click-down fs-2' onClick={() => {
+
+                                                        userStore.tryUpdate({
+                                                        // eslint-disable-next-line react/prop-types
+                                                            cart: userStore.userData.cart.concat(row.original.uuid)
+                                                        });
+                                                    }}>
+                                                    <BsCartPlus className='fs-2 ps-2 cursor-pointer' />
+                                                </button>
+                                            </OverlayTrigger>
+                                            <OverlayTrigger overlay={<Tooltip>Accept Suggested Label</Tooltip>}>
+                                                <button
+                                                    className='d-flex text-dark border-0 bg-transparent click-down fs-2'
+                                                >
+                                                    <BsPatchCheck className='fs-2 ps-2 cursor-pointer' />
+                                                </button>
+                                            </OverlayTrigger>
+                                            <OverlayTrigger overlay={<Tooltip>Reject Suggested Label</Tooltip>}>
+                                                <button
+                                                    className='d-flex text-dark border-0 bg-transparent click-down fs-2'
+                                                >
+                                                    <BsHandThumbsDown className='fs-2 ps-2 cursor-pointer' />
+                                                </button>
+                                            </OverlayTrigger>
+                                            <OverlayTrigger overlay={<Tooltip>Delete Datapoint</Tooltip>}>
+                                                <button
+                                                    className='d-flex text-dark border-0 bg-transparent click-down fs-2'
+                                                >
+                                                    <AiOutlineDelete className='fs-2 ps-2 cursor-pointer' />
+                                                </button>
+                                            </OverlayTrigger>
+                                        </div>
+                                    )
+                                }]}
+                                data={datapoints}
+                            />
+                        </>
                     )}
                 />
             </div>
