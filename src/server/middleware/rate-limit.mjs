55b428467321beae8fa1a7b5a553df1bf665749b
Jacques Arnoux <jacques@dioptra.ai>
2023-01-06 16:33:33 +0100

    rate-limit per api key or cookie session

--- src/server/middleware/rate-limit.mjs
+++ src/server/middleware/rate-limit.mjs
@@ -2,17 +2,11 @@ import rateLimit from 'express-rate-limit';
 
 const rateLimitMiddleware = rateLimit({
     windowMs: 60 * 1000,
-    max: 100, // limit each IP to 100 requests per windowMs'
+    max: 100,
     standardHeaders: true,
     legacyHeaders: false,
-    message: 'Too many requests from this IP, please try again after 15 minutes',
-    handler: (req, res) => {
-        console.log(`Rate limit exceeded for IP: ${req.ip}, ${req.rateLimit}`);
-
-        res.status(429).json({
-            message: 'Too many requests from this IP, please try again after 15 minutes'
-        });
-    }
+    message: 'Too many requests, please try again later.',
+    keyGenerator: (req) => req.headers['x-api-key'] || req.sessionID
 });
 
 export default rateLimitMiddleware;
