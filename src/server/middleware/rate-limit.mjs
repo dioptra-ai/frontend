dac2b1fc50e3315d49e100dc151297e8b30f0f31
Jacques Arnoux <jacques@dioptra.ai>
2023-01-06 16:02:16 +0100

    logging rate-limit for debug

--- src/server/middleware/rate-limit.mjs
+++ src/server/middleware/rate-limit.mjs
@@ -5,7 +5,14 @@ const rateLimitMiddleware = rateLimit({
     max: 100, // limit each IP to 100 requests per windowMs'
     standardHeaders: true,
     legacyHeaders: false,
-    message: 'Too many requests from this IP, please try again after 15 minutes'
+    message: 'Too many requests from this IP, please try again after 15 minutes',
+    handler: (req, res) => {
+        console.log(`Rate limit exceeded for IP: ${req.ip}, ${req.rateLimit}`);
+
+        res.status(429).json({
+            message: 'Too many requests from this IP, please try again after 15 minutes'
+        });
+    }
 });
 
 export default rateLimitMiddleware;
