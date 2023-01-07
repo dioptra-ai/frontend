695f178c5cdba430650bb1622c290f9b4755476d
Jacques Arnoux <jacques@dioptra.ai>
2023-01-07 14:33:22 +0100

    rate-limits errors on /api

--- src/server/middleware/rate-limit.mjs
+++ src/server/middleware/rate-limit.mjs
@@ -1,12 +1,24 @@
+import express from 'express';
 import rateLimit from 'express-rate-limit';
 
-const rateLimitMiddleware = rateLimit({
-    windowMs: 60 * 1000,
-    max: 100,
-    standardHeaders: true,
-    legacyHeaders: false,
-    message: 'Too many requests, please try again later.',
-    keyGenerator: (req) => req.headers['x-api-key'] || req.sessionID
-});
+const rateLimitMiddleware = express.Router().use(
+    rateLimit({
+        windowMs: 60 * 1000,
+        max: 100,
+        standardHeaders: true,
+        legacyHeaders: false,
+        message: 'Too many requests, please try again later.',
+        keyGenerator: (req) => req.headers['x-api-key'] || req.sessionID
+    }),
+    rateLimit({
+        windowMs: 60 * 1000,
+        max: 5,
+        standardHeaders: true,
+        legacyHeaders: false,
+        message: 'Too many errors, please try again later.',
+        skipSuccessfulRequests: true,
+        keyGenerator: (req) => req.headers['x-api-key'] || req.sessionID
+    })
+);
 
 export default rateLimitMiddleware;
