0832c807e96b5cf06be60e884195be27f97ee7e7
Jacques Arnoux <jacques@dioptra.ai>
2023-01-11 20:04:02 -0800

    removed the error rate-limiter

--- src/server/middleware/rate-limit.mjs
+++ src/server/middleware/rate-limit.mjs
@@ -4,19 +4,10 @@ import rateLimit from 'express-rate-limit';
 const rateLimitMiddleware = express.Router().use(
     rateLimit({
         windowMs: 60 * 1000,
-        max: 100,
+        max: 1000,
         standardHeaders: true,
         legacyHeaders: false,
         message: 'Too many requests, please try again later.',
-        keyGenerator: (req) => req.headers['x-api-key'] || req.sessionID
-    }),
-    rateLimit({
-        windowMs: 60 * 1000,
-        max: 5,
-        standardHeaders: true,
-        legacyHeaders: false,
-        message: 'Too many errors, please try again later.',
-        skipSuccessfulRequests: true,
         keyGenerator: (req) => req.headers['x-api-key'] || req.sessionID
     })
 );
