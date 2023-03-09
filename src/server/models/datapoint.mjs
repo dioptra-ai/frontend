db5780dc4c05608cad5dfe7f683687810d65498e
Jacques Arnoux <jacques@dioptra.ai>
2023-03-08 20:45:46 -0800

    now accomodating jsonb fields (metadata)

--- src/server/models/datapoint.mjs
+++ src/server/models/datapoint.mjs
@@ -7,8 +7,22 @@ const SAFE_OPS = new Set(['=', '!=', '<', '>', '<=', '>=', 'LIKE', 'NOT LIKE', '
 const isSafeOp = (op) => SAFE_OPS.has(op.toUpperCase());
 const SAFE_IDENTIFIERS = new Set(['*', 'RANDOM()']);
 const isSafeIdentifier = (identifier) => SAFE_IDENTIFIERS.has(identifier.toUpperCase());
+const JSONB_ROOT_COLUMNS = new Set(['metadata']);
+const isJSONBRootColumn = (column) => JSONB_ROOT_COLUMNS.has(column);
 
-const getCanonicalColumn = (column) => column.indexOf('.') === -1 ? `datapoints.${column}` : column;
+const getCanonicalColumn = (column) => {
+    const paths = column.split('.');
+
+    switch (paths[0]) {
+    case 'tags':
+    case 'predictions':
+    case 'groundtruths':
+    case 'datapoints':
+        return column;
+    default:
+        return `datapoints.${column}`;
+    }
+};
 const getCanonicalFilters = (filters) => filters.map((filter) => ({
     'left': getCanonicalColumn(filter['left']),
     'op': filter['op'],
@@ -16,9 +30,33 @@ const getCanonicalFilters = (filters) => filters.map((filter) => ({
 }));
 
 export const getCanonicalColumnTable = (column) => column.split('.')[0];
-export const getSafeColumn = (column) => {
+export const getSafeColumn = (column, withJSONBAlias = false) => {
+    const path = column.split('.');
+    const isChildOfJSONB = (i) => {
+        if (i === 0) {
+            return false;
+        } else {
+            return isJSONBRootColumn(path[i - 1]) || isChildOfJSONB(i - 1);
+        }
+    };
 
-    return pgFormat(column.split('.').map((c) => isSafeIdentifier(c) ? '%s' : '%I').join('.'), ...column.split('.'));
+    return pgFormat(
+        path.map((c, i) => {
+
+            if (isSafeIdentifier(c)) {
+                return i === 0 ? '%s' : '.%s';
+            } else if (isChildOfJSONB(i)) {
+                if (withJSONBAlias && i === path.length - 1) {
+                    return `->>%L AS ${pgFormat.ident(column)}`;
+                } else {
+                    return '->>%L';
+                }
+            } else {
+                return i === 0 ? '%I' : '.%I';
+            }
+        }).join(''),
+        ...path
+    );
 };
 const getSafeWhere = (canonicalFilters) => {
 
@@ -66,23 +104,25 @@ class Datapoint {
     }
 
     static async count({organizationId, filters = []}) {
-        assert(organizationId, 'organizationId is required');
-
-        const canonicalFilters = getCanonicalFilters(filters.concat({
-            'left': 'organization_id',
-            'op': '=',
-            'right': organizationId
-        }));
-        const canonicalFilterTables = canonicalFilters.map((filter) => getCanonicalColumnTable(filter['left']));
-
-        const {rows} = await postgresClient.query(
-            pgFormat(`SELECT COUNT(DISTINCT datapoints.id) as count ${getSafeFrom(canonicalFilterTables)} WHERE ${getSafeWhere(canonicalFilters)}`)
-        );
+        const {rows} = await postgresClient.query(`
+            SELECT COUNT(DISTINCT id) as count
+            FROM (${Datapoint._getSafeSelectQuery({
+        selectColumns: ['id'],
+        organizationId,
+        filters
+    })}) AS datapoints
+        `);
 
         return Number(rows[0]['count']);
     }
 
-    static async select({
+    static async select(...args) {
+        const {rows} = await postgresClient.query(Datapoint._getSafeSelectQuery(...args));
+
+        return rows;
+    }
+
+    static _getSafeSelectQuery({
         organizationId, selectColumns = [], filters = [],
         orderBy = 'datapoints.created_at', desc = false, limit = 1000000, offset = 0
     }) {
@@ -106,7 +146,7 @@ class Datapoint {
         }, {});
         const safeSelects = Object.entries(canonicalSelectColumnsPerTable).reduce((acc, [tableName, columns]) => {
             if (tableName === 'datapoints') {
-                acc.push(...columns.map(getSafeColumn));
+                acc.push(...columns.map((c) => getSafeColumn(c, true)));
             } else {
                 // Aggregate distinct JSONB objects with column values of tableName.
                 // Example: JSONB_BUILD_OBJECT('class_name', predictions.class_name, 'confidence', predictions.confidence, ...)
@@ -118,7 +158,7 @@ class Datapoint {
                         ARRAY_REMOVE(
                             ARRAY_AGG(DISTINCT 
                                 JSONB_BUILD_OBJECT(
-                                    ${columns.map((column) => [pgFormat.literal(column.split('.').slice(1)), getSafeColumn(column)]).flat().join(', ')}
+                                    ${columns.map((column) => [pgFormat.literal(column.split('.').slice(1)), getSafeColumn(column, true)]).flat().join(', ')}
                                 )
                             ), '{${columns.map((c) => pgFormat.literal(c.split('.').slice(1).join('.')).replaceAll('\'', '')).map((cc) => `"${cc}": null`)}}'
                         ) AS %I`, tableName)
@@ -130,7 +170,7 @@ class Datapoint {
         const canonicalSelectTables = canonicalSelectColumns.map(getCanonicalColumnTable);
         const canonicalFilterTables = canonicalFilters.map((filter) => getCanonicalColumnTable(filter['left']));
 
-        const {rows} = await postgresClient.query(`
+        return `
             SELECT ${safeSelects.join(', ')}
             ${getSafeFrom(canonicalSelectTables)}
             WHERE datapoints.id IN (
@@ -145,9 +185,7 @@ class Datapoint {
                 ) as filtered_datapoints
             )
             GROUP BY datapoints.id
-        `);
-
-        return rows;
+        `;
     }
 
     static async upsertByEventUuids(organizationId, eventUuids) {
@@ -180,13 +218,9 @@ class Datapoint {
     }
 
     static async deleteByFilters(organizationId, filters) {
-        const canonicalFilters = getCanonicalFilters(filters.concat({
-            'left': 'organization_id',
-            'op': '=',
-            'right': organizationId
-        }));
         const {rows} = await postgresClient.query(
-            `DELETE FROM datapoints WHERE ${getSafeWhere(canonicalFilters)} RETURNING *`
+            `DELETE FROM datapoints WHERE organization_id = $1 AND id IN (${Datapoint._getSafeSelectQuery({organizationId, filters, selectColumns: ['datapoints.id']})}) RETURNING *`,
+            [organizationId]
         );
 
         return rows;
