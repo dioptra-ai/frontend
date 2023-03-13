1af3180650d28336b265e7ea0b27c721958a4fe4
Jacques Arnoux <jacques@dioptra.ai>
2023-03-12 17:10:53 -0700

    fixes dataset selector

--- src/client/pages/dataset/dataset-selector.js
+++ src/client/pages/dataset/dataset-selector.js
@@ -11,9 +11,9 @@ import baseJSONClient from 'clients/base-json-client';
 
 const NO_ID = '';
 
-const DatasetSelector = ({as = 'a', title = 'Select Dataset', onChange, defaultValue, children = 'Select dataset', allowNew, ...rest}) => {
+const _DatasetSelector = ({as = 'a', title = 'Select Dataset', onChange, defaultValue, children = 'Select dataset', allowNew, datasets, ...rest}) => {
     const [modalOpen, setModalOpen] = useState(false);
-    const [selectedDataset, setSelectedDataset] = useState(defaultValue);
+    const [selectedDataset, setSelectedDataset] = useState(defaultValue || datasets[0]?.['uuid'] || NO_ID);
     const handleSubmit = async (event) => {
         event.stopPropagation();
         event.preventDefault();
@@ -42,58 +42,64 @@ const DatasetSelector = ({as = 'a', title = 'Select Dataset', onChange, defaultV
                 }, children)
             }
             <Modal isOpen={modalOpen} onClose={() => setModalOpen(false)} title={title}>
-                <Async
-                    fetchData={() => baseJSONClient('/api/dataset')}
-                    renderData={(datasets) => (
-                        <LoadingForm onSubmit={handleSubmit}>
-                            <Form.Group className='mb-3'>
-                                <Select name='datasetId' defaultValue={datasets[0]?.['uuid'] || selectedDataset} onChange={(value) => {
-                                    setSelectedDataset(value);
-                                }}>
-                                    {datasets.map((dataset) => (
-                                        <option key={dataset['uuid']} value={dataset['uuid']}>
-                                            {dataset['display_name']}
-                                        </option>
-                                    ))}
-                                    {
-                                        allowNew && (
-                                            <option value={NO_ID}>{'<new dataset>'}</option>
-                                        )
-                                    }
-                                </Select>
-                                {
-                                    selectedDataset && (
-                                        <a href={`/dataset/${selectedDataset}`} target='_blank' rel='noopener noreferrer' className='fs-6'>
-                                            {datasets.find((d) => d['uuid'] === selectedDataset)['display_name']}<FiExternalLink />
-                                        </a>
-                                    )
-                                }
-                            </Form.Group>
-                            {selectedDataset ? null : (
-                                <Form.Group className='mb-3'>
-                                    <Form.Label>New Dataset Name</Form.Label>
-                                    <Form.Control type='text' name='displayName' required/>
-                                </Form.Group>
-                            )}
-                            <LoadingForm.Error/>
-                            <LoadingForm.Button variant='primary' type='submit' className='text-white w-100'>
-                                Submit
-                            </LoadingForm.Button>
-                        </LoadingForm>
+                <LoadingForm onSubmit={handleSubmit}>
+                    <Form.Group className='mb-3'>
+                        <Select name='datasetId' value={selectedDataset} onChange={setSelectedDataset}>
+                            {datasets.map((dataset) => (
+                                <option key={dataset['uuid']} value={dataset['uuid']}>
+                                    {dataset['display_name']}
+                                </option>
+                            ))}
+                            {
+                                allowNew && (
+                                    <option value={NO_ID}>{'<new dataset>'}</option>
+                                )
+                            }
+                        </Select>
+                        {
+                            selectedDataset && (
+                                <a href={`/dataset/${selectedDataset}`} target='_blank' rel='noopener noreferrer' className='fs-6'>
+                                    preview <FiExternalLink />
+                                </a>
+                            )
+                        }
+                    </Form.Group>
+                    {selectedDataset ? null : (
+                        <Form.Group className='mb-3'>
+                            <Form.Label>New Dataset Name</Form.Label>
+                            <Form.Control type='text' name='displayName' required/>
+                        </Form.Group>
                     )}
-                />
+                    <LoadingForm.Error/>
+                    <LoadingForm.Button variant='primary' type='submit' className='text-white w-100'>
+                        Submit
+                    </LoadingForm.Button>
+                </LoadingForm>
             </Modal>
         </>
     );
 };
 
-DatasetSelector.propTypes = {
+_DatasetSelector.propTypes = {
     as: PropTypes.string,
     title: PropTypes.string,
     children: PropTypes.node.isRequired,
     onChange: PropTypes.func.isRequired,
     defaultValue: PropTypes.string,
-    allowNew: PropTypes.bool
+    allowNew: PropTypes.bool,
+    datasets: PropTypes.arrayOf(PropTypes.shape({
+        uuid: PropTypes.string.isRequired,
+        display_name: PropTypes.string.isRequired
+    })).isRequired
 };
+
+const DatasetSelector = (props) => (
+    <Async
+        fetchData={() => baseJSONClient('/api/dataset')}
+        renderData={(datasets) => (
+            <_DatasetSelector datasets={datasets} {...props} />
+        )}
+    />
+);
 
 export default DatasetSelector;
