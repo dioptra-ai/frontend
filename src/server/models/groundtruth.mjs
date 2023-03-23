bb82b7c0a043ab87cd7222f8a258f588ef102934
Jacques Arnoux <jacques@dioptra.ai>
2023-03-23 12:44:49 -0700

    enabling select for datapoints, predictions, groundtruths

--- src/server/models/groundtruth.mjs
+++ src/server/models/groundtruth.mjs
@@ -1,29 +1,6 @@
-import {postgresClient} from './index.mjs';
-
 class Groundtruth {
-    static async findByDatapointIds(organizationId, datapointIds) {
-        if (!datapointIds.length) {
-
-            return [];
-        } else {
-            const {rows} = await postgresClient.query(
-                `SELECT id, datapoint, task_type, class_name, class_names, 
-                        segmentation_class_mask, top, "left", height, width
-                 FROM groundtruths WHERE organization_id = $1 AND datapoint = ANY($2)`,
-                [organizationId, datapointIds]
-            );
-
-            return rows;
-        }
-    }
-
-    static async deleteByIds(organizationId, ids) {
-        const {rows} = await postgresClient.query(
-            'DELETE FROM groundtruths WHERE id = ANY($1) AND organization_id = $2 RETURNING *',
-            [ids, organizationId]
-        );
-
-        return rows;
+    static getTableName() {
+        return 'groundtruths';
     }
 }
 
--- src/server/models/prediction.mjs
+++ src/server/models/prediction.mjs
@@ -1,30 +1,7 @@
-import {postgresClient} from './index.mjs';
-
-class Prediction {
-    static async findByDatapointIds(organizationId, datapointIds) {
-        if (!datapointIds.length) {
-
-            return [];
-        } else {
-            const {rows} = await postgresClient.query(
-                `SELECT id, datapoint, task_type, class_name, class_names,
-                    confidence, confidences, segmentation_class_mask, top, "left", height, width, metrics, model_name
-                 FROM predictions WHERE organization_id = $1 AND datapoint = ANY($2)`,
-                [organizationId, datapointIds]
-            );
-
-            return rows;
-        }
-    }
-
-    static async deleteByIds(organizationId, ids) {
-        const {rows} = await postgresClient.query(
-            'DELETE FROM predictions WHERE id = ANY($1) AND organization_id = $2 RETURNING *',
-            [ids, organizationId]
-        );
-
-        return rows;
+class Groundtruth {
+    static getTableName() {
+        return 'groundtruths';
     }
 }
 
-export default Prediction;
+export default Groundtruth;
