15df6790ac8ef386aa812ae3c2c42fd6f9d082a1
Jacques Arnoux <jacques@dioptra.ai>
2023-05-10 13:44:30 -0700

    added birds-eye view for lane-detection

--- src/client/components/datapoints-viewer/datapoint.js
+++ src/client/components/datapoints-viewer/datapoint.js
@@ -43,120 +43,153 @@ const Datapoint = ({datapoint = {}, onClick, zoomable, showDetails, maxHeight}) 
         const someHeatMap = predictions.some((p) => p.bboxes?.some((b) => b['feature_heatmap']));
 
         return (
-            <Row onClick={onClick} className='g-2'>
-                <Col xs={12} style={{position: 'relative'}}>
-                    <TransformWrapper disabled={!zoomable}>
-                        {({zoomIn, zoomOut, resetTransform}) => (
-                            <div className={`${onClick ? 'cursor-pointer' : zoomable ? 'cursor-grab' : ''} d-flex flex-column align-items-center overflow-hidden`} style={{backgroundColor: '#18191B'}}>
-                                {zoomable ? (
-                                    <div className='position-absolute bg-white px-2 d-flex align-items-center' style={{zIndex: 1, top: -1, left: -1}}>
-                                        <TbZoomOut className='cursor-pointer fs-2' onClick={() => zoomOut()} />
-                                        <TbZoomIn className='cursor-pointer fs-2' onClick={() => zoomIn()} />
-                                        <TbZoomCancel className='cursor-pointer fs-2' onClick={() => resetTransform()} />
-                                        {someHeatMap ? (
-                                            <Form.Label className='mb-0 d-flex cursor-pointer'>
-                                                <Form.Check type='checkbox'
-                                                    onChange={(e) => setShowHeatMap(e.target.checked)}
-                                                    checked={showHeatMap}
-                                                />
-                                                    HeatMap
-                                            </Form.Label>
-                                        ) : null}
-                                        {predictions.length ? (
-                                            <Form.Label className='mb-0 me-2 d-flex cursor-pointer'>
-                                                <Form.Check type='checkbox'
-                                                    onChange={(e) => {
-                                                        setShowPredictions(e.target.checked);
-                                                    }}
-                                                    checked={showPredictions}
-                                                />
-                                                    Show Predictions
-                                            </Form.Label>
-                                        ) : null}
-                                        {groundtruths.length ? (
-                                            <Form.Label className='mb-0 d-flex cursor-pointer'>
-                                                <Form.Check type='checkbox'
-                                                    onChange={(e) => {
-                                                        setShowGroundtruths(e.target.checked);
-                                                    }}
-                                                    checked={showGroundtruths}
-                                                />
-                                                    Show Groundtruths
-                                            </Form.Label>
-                                        ) : null}
-                                    </div>
-                                ) : null}
-                                <TransformComponent wrapperStyle={{overflow: 'visible'}}>
-                                    <SignedImage
-                                        rawUrl={metadata['uri']}
-                                        onLoad={() => setViewportLoaded(true)}
-                                        onUnload={() => setViewportLoaded(false)}
-                                        style={{maxHeight: maxHeight || '100%', maxWidth: '100%'}}
-                                    />
-                                    {/* eslint-disable-next-line react/no-unknown-property */}
-                                    <style>{`
-                                        .hover-fade:hover {
-                                            opacity: 0.4;
-                                            transition: opacity 0.2s ease-in-out;
+            <>
+                <Row onClick={onClick} className='g-2'>
+                    <Col className='d-flex align-items-center justify-content-center' style={{position: 'relative'}}>
+                        <TransformWrapper disabled={!zoomable}>
+                            {({zoomIn, zoomOut, resetTransform}) => (
+                                <div className={`${onClick ? 'cursor-pointer' : zoomable ? 'cursor-grab' : ''} d-flex flex-column align-items-center overflow-hidden`} style={{backgroundColor: '#18191B'}}>
+                                    {zoomable ? (
+                                        <div className='position-absolute bg-white px-2 d-flex align-items-center' style={{zIndex: 1, top: -1, left: -1}}>
+                                            <TbZoomOut className='cursor-pointer fs-2' onClick={() => zoomOut()} />
+                                            <TbZoomIn className='cursor-pointer fs-2' onClick={() => zoomIn()} />
+                                            <TbZoomCancel className='cursor-pointer fs-2' onClick={() => resetTransform()} />
+                                            {someHeatMap ? (
+                                                <Form.Label className='mb-0 d-flex cursor-pointer'>
+                                                    <Form.Check type='checkbox'
+                                                        onChange={(e) => setShowHeatMap(e.target.checked)}
+                                                        checked={showHeatMap}
+                                                    />
+                                                        HeatMap
+                                                </Form.Label>
+                                            ) : null}
+                                            {predictions.length ? (
+                                                <Form.Label className='mb-0 me-2 d-flex cursor-pointer'>
+                                                    <Form.Check type='checkbox'
+                                                        onChange={(e) => {
+                                                            setShowPredictions(e.target.checked);
+                                                        }}
+                                                        checked={showPredictions}
+                                                    />
+                                                        Show Predictions
+                                                </Form.Label>
+                                            ) : null}
+                                            {groundtruths.length ? (
+                                                <Form.Label className='mb-0 d-flex cursor-pointer'>
+                                                    <Form.Check type='checkbox'
+                                                        onChange={(e) => {
+                                                            setShowGroundtruths(e.target.checked);
+                                                        }}
+                                                        checked={showGroundtruths}
+                                                    />
+                                                        Show Groundtruths
+                                                </Form.Label>
+                                            ) : null}
+                                        </div>
+                                    ) : null}
+                                    <TransformComponent wrapperStyle={{overflow: 'visible'}}>
+                                        <SignedImage
+                                            rawUrl={metadata['uri']}
+                                            onLoad={() => setViewportLoaded(true)}
+                                            onUnload={() => setViewportLoaded(false)}
+                                            style={{
+                                                height: '100%',
+                                                maxHeight: maxHeight || '100%',
+                                                maxWidth: '100%'
+                                            }}
+                                        />
+                                        {/* eslint-disable-next-line react/no-unknown-property */}
+                                        <style>{`
+                                            .hover-fade:hover {
+                                                opacity: 0.4;
+                                                transition: opacity 0.2s ease-in-out;
+                                            }
+                                        `}</style>
+                                        {
+                                            showPredictions && viewportLoaded ? predictions.map((p, i) => (
+                                                <>
+                                                    {
+                                                        p['encoded_resized_segmentation_class_mask'] ? (
+                                                            <div key={`pred-${i}`} className='position-absolute h-100 w-100'>
+                                                                <SegmentationMask encodedMask={p['encoded_resized_segmentation_class_mask']} classNames={p['class_names']} />
+                                                            </div>
+                                                        ) : null
+                                                    }
+                                                    {
+                                                        p.bboxes?.map((bbox, j) => <BBox imageWidth={imageW} imageHeight={imageH} bbox={bbox} key={j} showHeatMap={showHeatMap} />)
+                                                    }
+                                                    {
+                                                        p.lanes?.map((lane, j) => <Lane imageWidth={imageW} imageHeight={imageH} lane={lane} key={j} color={getHexColor(p['model_name'])} />)
+                                                    }
+                                                </>
+                                            )) : null
                                         }
-                                    `}</style>
-                                    {
-                                        showPredictions && viewportLoaded ? predictions.map((p, i) => (
-                                            <>
-                                                {
-                                                    p['encoded_resized_segmentation_class_mask'] ? (
-                                                        <div key={`pred-${i}`} className='position-absolute h-100 w-100'>
-                                                            <SegmentationMask encodedMask={p['encoded_resized_segmentation_class_mask']} classNames={p['class_names']} />
-                                                        </div>
-                                                    ) : null
-                                                }
-                                                {
-                                                    p.bboxes?.map((bbox, j) => <BBox imageWidth={imageW} imageHeight={imageH} bbox={bbox} key={j} showHeatMap={showHeatMap} />)
-                                                }
-                                                {
-                                                    p.lanes?.map((lane, j) => <Lane imageWidth={imageW} imageHeight={imageH} lane={lane} key={j} color={getHexColor(p['model_name'])} />)
-                                                }
-                                            </>
-                                        )) : null
-                                    }
-                                    {
-                                        showGroundtruths && viewportLoaded ? groundtruths.map((g, i) => (
-                                            <>
-                                                {
-                                                    g['encoded_resized_segmentation_class_mask'] ? (
-                                                        <div key={`gt-${i}`} className='position-absolute h-100 w-100'>
-                                                            <SegmentationMask encodedMask={g['encoded_resized_segmentation_class_mask']} classNames={g['class_names']} />
-                                                        </div>
-                                                    ) : null
-                                                }
-                                                {
-                                                    g.bboxes?.map((bbox, j) => <BBox imageWidth={imageW} imageHeight={imageH} bbox={bbox} key={j} showHeatMap={showHeatMap} />)
-                                                }
-                                                {
-                                                    g.lanes?.map((lane, j) => <Lane imageWidth={imageW} imageHeight={imageH} lane={lane} key={j} color={getHexColor(g['task_type'])} />)
-                                                }
-                                            </>
-                                        )) : null
-                                    }
-                                </TransformComponent>
-                            </div>
-                        )}
-                    </TransformWrapper>
-                </Col>
-                {showDetails && Object.keys(averageConfidencePerClass).length > 0 ? (
-                    <Col xs={12}>
-                        <PerformanceBox title='Confidence' minHeight={50} data={Object.entries(averageConfidencePerClass).map(([label, value]) => ({label, value}))} />
+                                        {
+                                            showGroundtruths && viewportLoaded ? groundtruths.map((g, i) => (
+                                                <>
+                                                    {
+                                                        g['encoded_resized_segmentation_class_mask'] ? (
+                                                            <div key={`gt-${i}`} className='position-absolute h-100 w-100'>
+                                                                <SegmentationMask encodedMask={g['encoded_resized_segmentation_class_mask']} classNames={g['class_names']} />
+                                                            </div>
+                                                        ) : null
+                                                    }
+                                                    {
+                                                        g.bboxes?.map((bbox, j) => <BBox imageWidth={imageW} imageHeight={imageH} bbox={bbox} key={j} showHeatMap={showHeatMap} />)
+                                                    }
+                                                    {
+                                                        g.lanes?.map((lane, j) => <Lane imageWidth={imageW} imageHeight={imageH} lane={lane} key={j} color={getHexColor(g['task_type'])} />)
+                                                    }
+                                                </>
+                                            )) : null
+                                        }
+                                    </TransformComponent>
+                                </div>
+                            )}
+                        </TransformWrapper>
                     </Col>
-                ) : null}
+                    {
+                        showDetails && predictions?.some((p) => p['task_type'] === 'LANE_DETECTION') ? (
+                            <Col
+                                className='d-flex align-items-center justify-content-center position-relative'
+                                style={{maxHeight: '100%', maxWidth: '100%'}}
+                            >
+                                <img
+                                    style={{maxHeight: 80}}
+                                    src='https://dioptra-public.s3.us-east-2.amazonaws.com/1126190088-612x612.jpg'
+                                />
+                                <div
+                                    className='position-absolute d-flex justify-content-center pt-4'
+                                    style={{
+                                        top: 0, left: 0, width: '100%', height: '100%',
+                                        backgroundColor: 'rgba(255, 255, 255, 0.5)',
+                                        color: 'rgba(0, 0, 0, 0.2)'
+                                    }}
+                                >
+                                    No Birds-Eye View Prediction Found
+                                </div>
+                            </Col>
+                        ) : null
+                    }
+                </Row>
                 {
                     showDetails ? (
-                        <Col xs={12}>
-                            <hr />
-                            <RenderDatapoint datapoint={datapoint} />
-                        </Col>
+                        <Row className='g-2'>
+                            {
+                                Object.keys(averageConfidencePerClass).length > 0 ? (
+                                    <Col>
+                                        <PerformanceBox title='Confidence' minHeight={50} data={Object.entries(averageConfidencePerClass).map(([label, value]) => ({label, value}))} />
+                                    </Col>
+                                ) : null
+                            }
+                            <Col xs={12}>
+                                <hr />
+                                <RenderDatapoint datapoint={datapoint} />
+                            </Col>
+                        </Row>
                     ) : null
                 }
-            </Row>
+            </>
         );
     }
     default:
