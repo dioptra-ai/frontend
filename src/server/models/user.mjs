3c8390fba657c0591b8f7384b3e02f71789ebbf1
Jacques Arnoux <jacques@dioptra.ai>
2023-05-13 23:11:17 -0700

    removed email validation from user creation flow

--- src/server/models/user.mjs
+++ src/server/models/user.mjs
@@ -1,4 +1,3 @@
-import fetch from 'node-fetch';
 import mongoose from 'mongoose';
 import bcrypt from 'bcrypt';
 import autopopulate from 'mongoose-autopopulate';
@@ -120,44 +119,6 @@ userSchema.statics.createAsMemberOf = async (userProps, organization) => {
 
     return newUser;
 };
-
-userSchema.pre('save', async function () {
-
-    if (this.isNew || this.isModified('username')) { // eslint-disable-line no-invalid-this
-        let status = 'valid';
-
-        try {
-            const response = await fetch(`https://api.hunter.io/v2/email-verifier?email=${this['username']}&api_key=29073794876a97a92ba2b8a1b45291305a2350af`); // eslint-disable-line no-invalid-this
-            const jsonResponse = await response.json();
-
-            if (response.ok) {
-                status = jsonResponse['data']['status'];
-            } else {
-                const error = jsonResponse['errors'][0];
-
-                if (error.code === 400) {
-                    status = 'invalid';
-                }
-
-                throw new Error(error['details']);
-            }
-        } catch (e) {
-            console.warn(`Error when verifying email address: ${e.message}`);
-        }
-
-        // see: https://hunter.io/api-documentation/v2#email-verifier
-        // "valid": the email address is valid.
-        // "invalid": the email address is not valid.
-        // "accept_all": the email address is valid but any email address is accepted by the server.
-        // "webmail": the email address comes from an email service provider such as Gmail or Outlook.
-        // "disposable": the email address comes from a disposable email service provider.
-        // "unknown": we failed to verify the email address.
-        if (status === 'invalid' || status === 'disposable' || status === 'webmail') {
-
-            throw new Error('We could not verify your email. Please use a valid work email.');
-        }
-    }
-});
 
 const User = mongoose.model('User', userSchema);
 
