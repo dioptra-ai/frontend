b8b85f1caf08079fb0bddc1b2eaf4eedbb00c630
Pierre <pierre@dioptra.ai>
2023-05-31 14:40:17 -0400

    Revert "using organization membership instead"
    This reverts commit afd6412d7193d350cf00080ca12931942a3e7616.

--- src/client/state/stores/user-store.js
+++ src/client/state/stores/user-store.js
@@ -43,7 +43,7 @@ class UserStore {
         if (data !== this._userData) {
             if (data) {
                 identify(data.username);
-                group(data.organizationMemberships?.organization?.name)
+                group(data.activeOrganizationMembership?.organization?.name)
             } else {
                 resetTracking();
             }
